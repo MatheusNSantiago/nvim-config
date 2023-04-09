d5e2e6c3c76882234a7d3e68065ba4ccdba6b4b6
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-09 04:41:08 -0300

    add keymaps for unstaging in git fugitive

--- lua/config/plugins/git/vim-fugitive.lua
+++ lua/config/plugins/git/vim-fugitive.lua
@@ -5,6 +5,10 @@ local M = {}
 function M.setup()
 	map("n", "<leader>gs", ":Git add %<CR>", { desc = "[g]it [s]tage current file" })
 	map("n", "<leader>gS", ":Git add .<CR>", { desc = "[G]it [S]tage all files" })
+
+	map("n", "<leader>gu", ":Git reset HEAD %<CR>", { desc = "[G]it [u]nstage current file" })
+	map("n", "<leader>gU", ":Git reset HEAD<CR>", { desc = "[G]it [U]nstage all files" })
+
 	map("n", "<leader>gc", ":Git commit<CR>", { desc = "[G]it [c]ommit" })
 	map("n", "<leader>gp", ":Git push<CR>", { desc = "[G]it [p]ush" })
 
