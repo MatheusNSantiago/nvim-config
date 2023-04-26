b0a0d5d05dc193ebcf75da180f1ab8ebcb55c054
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-26 14:19:47 -0300

    feat(indent-blankline): new line

--- lua/plugins/ui/indent-blankline.lua
+++ lua/plugins/ui/indent-blankline.lua
@@ -1,26 +1,46 @@
 local M = {}
 
 function M.setup()
-    return {
-        "lukas-reineke/indent-blankline.nvim",
-        config = M.config,
-    }
+  return {
+    "lukas-reineke/indent-blankline.nvim",
+    config = M.config,
+  }
 end
 
 function M.config()
   require("indent_blankline").setup({
-    char = "┊",
+    char = "│", -- ┊ ┆ 
+    context_char = "▎",
+    show_foldtext = false,
+    char_priority = 12,
     show_trailing_blankline_indent = false,
     buftype_exclude = { "terminal", "telescope", "nofile" },
-    filetype_exclude = { "help", "dashboard", "packer", "NvimTree", "Trouble", "TelescopePrompt", "Float" },
+    filetype_exclude = {
+      "dbout",
+      "neo-tree-popup",
+      "log",
+      "gitcommit",
+      "txt",
+      "help",
+      "NvimTree",
+      "git",
+      "flutterToolsOutline",
+      "undotree",
+      "markdown",
+      "norg",
+      "org",
+      "orgagenda",
+      "", -- for all buffers without a file type
+    },
     show_current_context = true,
-    show_current_context_start = false,
+    show_current_context_start = true,
+    show_current_context_start_on_current_line = false,
     show_end_of_line = false,
+    show_first_indent_level = true,
     space_char_blankline = " ",
     use_treesitter = true,
   })
 end
-
 
 M.highlights = {
   -- hl(0, "IndentBlanklineContextChar", { fg = c.context, bg = "NONE" })
