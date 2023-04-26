c05749bd061c868e05a2602d53eef7ec89b870cf
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-25 21:43:13 -0300

    refactor(tsserver)

--- lua/lsp/servers/tsserver.lua
+++ lua/lsp/servers/tsserver.lua
@@ -10,89 +10,79 @@ capabilities.textDocument.completion.completionItem.deprecatedSupport = true
 capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
 capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
 capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    "documentation",
-    "detail",
-    "additionalTextEdits",
-  },
+	properties = {
+		"documentation",
+		"detail",
+		"additionalTextEdits",
+	},
 }
 capabilities.textDocument.codeAction = {
-  dynamicRegistration = false,
-  codeActionLiteralSupport = {
-    codeActionKind = {
-      valueSet = {
-        "",
-        "quickfix",
-        "refactor",
-        "refactor.extract",
-        "refactor.inline",
-        "refactor.rewrite",
-        "source",
-        "source.organizeImports",
-      },
-    },
-  },
+	dynamicRegistration = false,
+	codeActionLiteralSupport = {
+		codeActionKind = {
+			valueSet = {
+				"",
+				"quickfix",
+				"refactor",
+				"refactor.extract",
+				"refactor.inline",
+				"refactor.rewrite",
+				"source",
+				"source.organizeImports",
+			},
+		},
+	},
 }
 capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
 }
 
 local function filter(arr, fn)
-  if type(arr) ~= "table" then
-    return arr
-  end
+	if type(arr) ~= "table" then
+		return arr
+	end
 
-  local filtered = {}
-  for k, v in pairs(arr) do
-    if fn(v, k, arr) then
-      table.insert(filtered, v)
-    end
-  end
+	local filtered = {}
+	for k, v in pairs(arr) do
+		if fn(v, k, arr) then
+			table.insert(filtered, v)
+		end
+	end
 
-  return filtered
+	return filtered
 end
 
 local function filterReactDTS(value)
-  -- Depending on typescript version either uri or targetUri is returned
-  if value.uri then
-    return string.match(value.uri, "%.d.ts") == nil
-  elseif value.targetUri then
-    return string.match(value.targetUri, "%.d.ts") == nil
-  end
+	-- Depending on typescript version either uri or targetUri is returned
+	if value.uri then
+		return string.match(value.uri, "%.d.ts") == nil
+	elseif value.targetUri then
+		return string.match(value.targetUri, "%.d.ts") == nil
+	end
 end
 
 local handlers = {
-  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-  ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-  ["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    { virtual_text = true }
-  ),
-  ["textDocument/definition"] = function(err, result, method, ...)
-    if vim.tbl_islist(result) and #result > 1 then
-      local filtered_result = filter(result, filterReactDTS)
-      return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
-    end
+	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+	["textDocument/publishDiagnostics"] = vim.lsp.with(
+		vim.lsp.diagnostic.on_publish_diagnostics,
+		{ virtual_text = true }
+	),
+	["textDocument/definition"] = function(err, result, method, ...)
+		if vim.tbl_islist(result) and #result > 1 then
+			local filtered_result = filter(result, filterReactDTS)
+			return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
+		end
 
-    vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
-  end,
+		vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
+	end,
 }
 
 local on_attach = function(client, bufnr)
-  local utils = require("lsp.utils")
+	require("lsp").common_on_attach(client, bufnr)
 
-  -- setup navic (breadcrumbs) e outros simbolos
-  utils.setup_document_symbols(client, bufnr)
-
-  -- Setup nav buddy
-  local navbuddy_ok, navbuddy = pcall(require, "nvim-navbuddy")
-
-  if navbuddy_ok then
-    navbuddy.attach(client, bufnr)
-  end
-
-  utils.map("n", "gd", ":TypescriptGoToSourceDefinition<CR>")
+	utils.map("n", "gd", ":TypescriptGoToSourceDefinition<CR>")
 end
 
 M.capabilities = capabilities
