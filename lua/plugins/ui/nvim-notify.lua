ee228a002c8c9d36beebb23a5bd120c93544bd70
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-27 23:12:18 -0300

    feat(nvim-notify): add new highlights

--- lua/plugins/ui/nvim-notify.lua
+++ lua/plugins/ui/nvim-notify.lua
@@ -45,22 +45,24 @@ function M.config()
 end
 
 M.highlights = {
-	-- hl(0, "NotifyERRORBorder", { fg = "#8A1F1F", bg = "NONE" })
-	-- hl(0, "NotifyWARNBorder", { fg = "#79491D", bg = "NONE" })
-	-- hl(0, "NotifyINFOBorder", { fg = c.ui_blue, bg = "NONE" })
-	-- hl(0, "NotifyDEBUGBorder", { fg = c.gray, bg = "NONE" })
-	-- hl(0, "NotifyTRACEBorder", { fg = "#4F3552", bg = "NONE" })
-	-- hl(0, "NotifyERRORIcon", { fg = c.error, bg = "NONE" })
-	-- hl(0, "NotifyWARNIcon", { fg = c.warn, bg = "NONE" })
-	-- hl(0, "NotifyINFOIcon", { fg = c.ui4_blue, bg = "NONE" })
-	-- hl(0, "NotifyDEBUGIcon", { fg = c.gray, bg = "NONE" })
-	-- hl(0, "NotifyTRACEIcon", { fg = c.ui_purple, bg = "NONE" })
-	-- hl(0, "NotifyERRORTitle", { fg = c.error, bg = "NONE" })
-	-- hl(0, "NotifyWARNTitle", { fg = c.warn, bg = "NONE" })
-	-- hl(0, "NotifyINFOTitle", { fg = c.ui4_blue, bg = "NONE" })
-	-- hl(0, "NotifyDEBUGTitle", { fg = c.gray, bg = "NONE" })
-	-- hl(0, "NotifyTRACETitle", { fg = c.ui_purple, bg = "NONE" })
-	--
+	NotifyERRORBorder = { link = "DiagnosticError" },
+	NotifyWARNBorder = { link = "DiagnosticWarn" },
+	NotifyINFOBorder = { link = "DiagnosticInfo" },
+	NotifyHINTBorder = { link = "DiagnosticHint" },
+	NotifyDEBUGBorder = { link = "Debug" },
+	NotifyTRACEBorder = { link = "Comment" },
+	NotifyERRORIcon = { link = "DiagnosticError" },
+	NotifyWARNIcon = { link = "DiagnosticWarn" },
+	NotifyINFOIcon = { link = "DiagnosticInfo" },
+	NotifyHINTIcon = { link = "DiagnosticHint" },
+	NotifyDEBUGIcon = { link = "Debug" },
+	NotifyTRACEIcon = { link = "Comment" },
+	NotifyERRORTitle = { link = "DiagnosticError" },
+	NotifyWARNTitle = { link = "DiagnosticWarn" },
+	NotifyINFOTitle = { link = "DiagnosticInfo" },
+	NotifyHINTTitle = { link = "DiagnosticHint" },
+	NotifyDEBUGTitle = { link = "Debug" },
+	NotifyTRACETitle = { link = "Comment" },
 }
 
 return M
