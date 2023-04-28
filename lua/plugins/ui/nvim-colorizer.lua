aec522a49776a564e4eead5eca807965cef4d9e9
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-27 22:07:47 -0300

    feat(nvim-colorizer): add options

--- lua/plugins/ui/nvim-colorizer.lua
+++ lua/plugins/ui/nvim-colorizer.lua
@@ -4,22 +4,24 @@ function M.setup()
 	local is_installed = pcall(require, "colorizer")
 
 	if is_installed then
-		vim.api.nvim_create_autocmd(
-			{"BufEnter", "BufWritePost"},
-			{ command = [[ColorizerAttachToBuffer]] }
-		)
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, { command = [[ColorizerAttachToBuffer]] })
 	end
 
 	return {
 		"norcalli/nvim-colorizer.lua",
-		-- event = "BufEnter",
-		-- cmd = { "ColorizerToggle", "ColorizerAttachToBuffer", "ColorizerDetachFromBuffer", "ColorizerReloadAllBuffers" },
 		config = M.config,
 	}
 end
 
 function M.config()
-	require("colorizer").setup({})
+	require("colorizer").setup({
+		filetypes = { "*" },
+		user_default_options = {
+			names = false,
+			tailwind = "both",
+			mode = "background",
+		},
+	})
 end
 
 return M
