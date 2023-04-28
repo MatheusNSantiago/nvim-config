8b9f3b6aa6eaacbbb226c5b4f421849d3dd00dc4
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-28 13:43:59 -0300

    feat(vim-autopairs): add triple quotes completion

--- lua/plugins/edit/vim-autopairs.lua
+++ lua/plugins/edit/vim-autopairs.lua
@@ -2,28 +2,27 @@ local M = {}
 
 function M.setup()
 	-- make autopairs and completion work together
-	local cmp_status_ok, cmp = pcall(require, "cmp")
-	if cmp_status_ok then
-		cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
-	end
+	local cmp_status_ok, cmp = pcall(require, 'cmp')
+	if cmp_status_ok then cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done()) end
 
 	return {
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
+		'windwp/nvim-autopairs',
+		event = 'InsertEnter',
 		config = M.config,
 	}
 end
 
 function M.config()
-	require("nvim-autopairs").setup({
+	require('nvim-autopairs').setup({
 		check_ts = true,
+		close_triple_quotes = true,
 		ts_config = {
-			lua = { "string", "source" },
-			javascript = { "string", "template_string" },
+			lua = { 'string', 'source' },
+			javascript = { 'string', 'template_string' },
 			java = false,
 		},
 		enable_check_bracket_line = false, -- (  |)) => ( => (  (|))
-		disable_filetype = { "TelescopePrompt", "vim" },
+		disable_filetype = { 'TelescopePrompt', 'vim' },
 	})
 end
 
