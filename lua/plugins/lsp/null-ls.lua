0070d578055959f5ddc98f5e23703f60dbad3899
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-27 21:31:19 -0300

    feat(null-ls): make format keymap more reliable

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -28,30 +28,42 @@ function M.config()
       b.formatting.fish_indent, -- fish
 
       --  ╭──────────────────────────────────────────────────────────╮
-      --  │                       Diagnostics                        │
+      --  │                         Linter                           │
       --  ╰──────────────────────────────────────────────────────────╯
 
       -- b.diagnostics.eslint_d, -- js/ts
       -- b.diagnostics.xo,
       b.diagnostics.fish,  -- fish
-      b.diagnostics.ruff,   -- python
+      b.diagnostics.ruff,  -- python
       b.diagnostics.yamllint, -- yaml
 
---  ╭──────────────────────────────────────────────────────────╮
---  │                        Completion                        │
---  ╰──────────────────────────────────────────────────────────╯
+      --  ╭──────────────────────────────────────────────────────────╮
+      --  │                        Completion                        │
+      --  ╰──────────────────────────────────────────────────────────╯
 
       b.completion.luasnip,
 
---  ╭──────────────────────────────────────────────────────────╮
---  │                       Code Actions                       │
---  ╰──────────────────────────────────────────────────────────╯
+      --  ╭──────────────────────────────────────────────────────────╮
+      --  │                       Code Actions                       │
+      --  ╰──────────────────────────────────────────────────────────╯
       b.code_actions.eslint_d,
       require("typescript.extensions.null-ls.code-actions"),
     },
     on_attach = function(client, bufnr)
+      local ft = vim.bo[bufnr].filetype
+      local have_nls = #require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0
+
       vim.keymap.set("n", "<Leader>ff", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+        vim.lsp.buf.format({
+          bufnr = bufnr,
+          timeout_ms = 5000,
+          filter = function(_client)
+            if have_nls then
+              return _client.name == "null-ls"
+            end
+            return _client.name ~= "null-ls"
+          end,
+        })
       end, { buffer = bufnr })
     end,
   })
