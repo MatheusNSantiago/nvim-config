77e2db9c3182d2a3c7aa4ed641e81cae15fe5981
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-28 10:00:35 -0300

    feat(autocommands): implement save before leaving buffer

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,88 +1,88 @@
-local api, cmd, fmt, fn = vim.api, vim.cmd, string.format, vim.fn
+local api, cmd, fn = vim.api, vim.cmd, vim.fn
 
 --  ╭──────────────────────────────────────────────────────────╮
 --  │               don't auto comment new line                │
 --  ╰──────────────────────────────────────────────────────────╯
-api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+api.nvim_create_autocmd('BufEnter', { command = [[set formatoptions-=cro]] })
 --  ╭──────────────────────────────────────────────────────────╮
 --  │           go to last loc when opening a buffer           │
 --  ╰──────────────────────────────────────────────────────────╯
 api.nvim_create_autocmd(
-	"BufReadPost",
+	'BufReadPost',
 	{ command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
 )
 --  ╭──────────────────────────────────────────────────────────╮
 --  │            Strip trailing spaces before write            │
 --  ╰──────────────────────────────────────────────────────────╯
-api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*" },
-	callback = function()
-		cmd([[ %s/\s\+$//e ]])
-	end,
+api.nvim_create_autocmd({ 'BufWritePre' }, {
+	pattern = { '*' },
+	callback = function() cmd([[ %s/\s\+$//e ]]) end,
 })
 --  ╭──────────────────────────────────────────────────────────╮
 --  │    Open images in an image viewer (probably Preview)     │
 --  ╰──────────────────────────────────────────────────────────╯
-utils.api.augroup("ExternalCommands", {
-	event = { "BufEnter" },
-	pattern = { "*.png", "*.jpg", "*.gif" },
+utils.api.augroup('ExternalCommands', {
+	event = { 'BufEnter' },
+	pattern = { '*.png', '*.jpg', '*.gif' },
 	command = function()
-		cmd("silent! !open " .. fn.expand("%"))
-		cmd("silent! :bw!")
+		cmd('silent! !open ' .. fn.expand('%'))
+		cmd('silent! :bw!')
+	end,
+})
+--  ╭──────────────────────────────────────────────────────────╮
+--  │                   Salva antes de sair                    │
+--  ╰──────────────────────────────────────────────────────────╯
+local save_excluded = { 'NvimTree', 'lua.luapad', 'gitcommit', 'NeogitCommitMessage' }
+local function can_save()
+	return utils.falsy(fn.win_gettype())
+		and utils.falsy(vim.bo.buftype)
+		and not utils.falsy(vim.bo.filetype)
+		and vim.bo.modifiable
+		and not vim.tbl_contains(save_excluded, vim.bo.filetype)
+end
+
+api.nvim_create_autocmd({ 'BufLeave' }, {
+	pattern = { '*' },
+	callback = function()
+		if can_save() then cmd('silent! write ++p') end
 	end,
 })
 --  ╭──────────────────────────────────────────────────────────╮
 --  │     Desligar o highlight da pesquisa automaticamente     │
 --  ╰──────────────────────────────────────────────────────────╯
-utils.map({ "n", "v", "o", "i", "c" }, "<Plug>(StopHL)", ':execute("nohlsearch")<CR>')
+utils.map({ 'n', 'v', 'o', 'i', 'c' }, '<Plug>(StopHL)', ':execute("nohlsearch")<CR>')
 
 local function stop_hl()
-	if vim.v.hlsearch == 0 or api.nvim_get_mode().mode ~= "n" then
-		return
-	end
-	api.nvim_feedkeys(utils.api.replace_termcodes("<Plug>(StopHL)"), "m", false)
+	if vim.v.hlsearch == 0 or api.nvim_get_mode().mode ~= 'n' then return end
+	api.nvim_feedkeys(utils.api.replace_termcodes('<Plug>(StopHL)'), 'm', false)
 end
 
 local function hl_search()
 	local col = api.nvim_win_get_cursor(0)[2]
 	local curr_line = api.nvim_get_current_line()
-	local ok, match = pcall(fn.matchstrpos, curr_line, fn.getreg("/"), 0)
-	if not ok then
-		return
-	end
+	local ok, match = pcall(fn.matchstrpos, curr_line, fn.getreg('/'), 0)
+	if not ok then return end
 	local _, p_start, p_end = unpack(match)
 	-- if the cursor is in a search result, leave highlighting on
-	if col < p_start or col > p_end then
-		stop_hl()
-	end
+	if col < p_start or col > p_end then stop_hl() end
 end
 
-utils.api.augroup("VimrcIncSearchHighlight", {
-	event = { "CursorMoved" },
+utils.api.augroup('VimrcIncSearchHighlight', {
+	event = { 'CursorMoved' },
+	command = function() hl_search() end,
+}, {
+	event = { 'InsertEnter' },
+	command = function() stop_hl() end,
+}, {
+	event = { 'OptionSet' },
+	pattern = { 'hlsearch' },
 	command = function()
-		hl_search()
+		vim.schedule(function() cmd.redrawstatus() end)
 	end,
 }, {
-	event = { "InsertEnter" },
-	command = function()
-		stop_hl()
-	end,
+	event = 'RecordingEnter',
+	command = function() vim.o.hlsearch = false end,
 }, {
-	event = { "OptionSet" },
-	pattern = { "hlsearch" },
-	command = function()
-		vim.schedule(function()
-			cmd.redrawstatus()
-		end)
-	end,
-}, {
-	event = "RecordingEnter",
-	command = function()
-		vim.o.hlsearch = false
-	end,
-}, {
-	event = "RecordingLeave",
-	command = function()
-		vim.o.hlsearch = true
-	end,
+	event = 'RecordingLeave',
+	command = function() vim.o.hlsearch = true end,
 })
