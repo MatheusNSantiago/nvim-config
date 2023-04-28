b26736d57c5843bdfdd51fddaf92d21900f481c6
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-28 10:23:40 -0300

    feat(treesitter): add custom argument text object

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,39 +2,39 @@ local M = {}
 
 function M.setup()
 	return {
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate',
 		config = M.config,
 	}
 end
 
 function M.config()
-	local ts = require("nvim-treesitter.configs")
+	local ts = require('nvim-treesitter.configs')
 
 	ts.setup({
 		ensure_installed = {
-			"vim",
-			"vimdoc",
-			"markdown",
-			"markdown_inline",
-			"bash",
-			"regex",
-			"javascript",
-			"typescript",
-			"prisma",
-			"ruby",
-			"tsx",
-			"python",
-			"dart",
-			"json",
-			"html",
-			"lua",
-			"css",
-			"scss",
-			"toml",
-			"fish",
-			"jsdoc",
-			"yaml",
+			'vim',
+			'vimdoc',
+			'markdown',
+			'markdown_inline',
+			'bash',
+			'regex',
+			'javascript',
+			'typescript',
+			'prisma',
+			'ruby',
+			'tsx',
+			'python',
+			'dart',
+			'json',
+			'html',
+			'lua',
+			'css',
+			'scss',
+			'toml',
+			'fish',
+			'jsdoc',
+			'yaml',
 		},
 		auto_install = true,
 		autotag = { enable = true },
@@ -45,8 +45,8 @@ function M.config()
 		incremental_selection = {
 			enable = true,
 			keymaps = {
-				init_selection = "<C-space>",
-				node_incremental = "<C-space>",
+				init_selection = '<C-space>',
+				node_incremental = '<C-space>',
 				-- scope_incremental = "<nop>",
 				-- node_decremental = "<bs>",
 			},
@@ -59,38 +59,38 @@ function M.config()
 		},
 		context_commentstring = { enable = true, enable_autocmd = false },
 		textobjects = {
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]]"] = "@function.outer",
-					["]m"] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[["] = "@function.outer",
-					["[m"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]["] = "@function.outer",
-					["]M"] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[]"] = "@function.outer",
-					["[M"] = "@class.outer",
-				},
-			},
 			select = {
 				enable = true,
 				-- Automatically jump forward to textobj, similar to targets.vim
 				lookahead = true,
 				keymaps = {
 					-- You can use the capture groups defined in textobjects.scm
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
+					['af'] = '@function.outer',
+					['if'] = '@function.inner',
+					['ac'] = '@class.outer',
+					['ic'] = '@class.inner',
 				},
 				include_surrounding_whitespace = false,
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					[']]'] = '@function.outer',
+					[']m'] = '@class.outer',
+				},
+				goto_previous_start = {
+					['[['] = '@function.outer',
+					['[m'] = '@class.outer',
+				},
+				goto_next_end = {
+					[']['] = '@function.outer',
+					[']M'] = '@class.outer',
+				},
+				goto_previous_end = {
+					['[]'] = '@function.outer',
+					['[M'] = '@class.outer',
+				},
 			},
 			swap = { enable = false, swap_next = {} },
 		},
