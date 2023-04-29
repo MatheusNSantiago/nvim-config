ae1af5209f6d6ba99b89e5f5e376d7dba4860f0f
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-29 18:29:51 -0300

    feat(nvim-colorizer): disable colorizer on NvimTree

--- lua/plugins/ui/nvim-colorizer.lua
+++ lua/plugins/ui/nvim-colorizer.lua
@@ -1,25 +1,32 @@
 local M = {}
 
 function M.setup()
-	local is_installed = pcall(require, "colorizer")
+	local is_installed = pcall(require, 'colorizer')
 
 	if is_installed then
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, { command = [[ColorizerAttachToBuffer]] })
+		utils.api.augroup('AttatchColorizer', {
+			event = { 'BufEnter', 'BufWritePost' },
+			pattern = '*',
+			command = function()
+				if vim.bo.filetype == 'NvimTree' then return end
+				return vim.cmd([[ColorizerAttachToBuffer]])
+			end,
+		})
 	end
 
 	return {
-		"norcalli/nvim-colorizer.lua",
+		'norcalli/nvim-colorizer.lua',
 		config = M.config,
 	}
 end
 
 function M.config()
-	require("colorizer").setup({
-		filetypes = { "*" },
+	require('colorizer').setup({
+		filetypes = { '*', '!NvimTree' },
 		user_default_options = {
 			names = false,
-			tailwind = "both",
-			mode = "background",
+			tailwind = 'both',
+			mode = 'background',
 		},
 	})
 end
