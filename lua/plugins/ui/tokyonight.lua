7dc278054cf04fba54ababe98485a2877e720288
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-04-29 14:22:43 -0300

    feat(colorscheme): set custom highlights when the theme loads

--- lua/plugins/ui/tokyonight.lua
+++ lua/plugins/ui/tokyonight.lua
@@ -2,30 +2,33 @@ local M = {}
 
 function M.setup()
 	return {
-		"folke/tokyonight.nvim",
+		'folke/tokyonight.nvim',
 		config = M.config,
 	}
 end
 
 function M.config()
-	local tokyonight = require("tokyonight")
+	local tokyonight = require('tokyonight')
 
 	tokyonight.setup({
-		style = "night",   -- night | storm | moon
+		style = 'night',   -- night | storm | moon
 		transparent = true, -- Enable this to disable setting the background color
 		terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
 		styles = {
 			-- Style to be applied to different syntax groups
 			-- Value is any valid attr-list value `:help attr-list`
-			comments = "italic",
-			keywords = "italic",
-			functions = "bold",
-			variables = "NONE",
+			comments = 'italic',
+			keywords = 'italic',
+			functions = 'bold',
+			variables = 'NONE',
 			-- Background styles. Can be "dark", "transparent" or "normal"
-			sidebars = "transparent", -- style for sidebars, see below
-			floats = "transparent", -- style for floating windows
+			sidebars = 'transparent', -- style for sidebars, see below
+			floats = 'transparent', -- style for floating windows
 		},
 		on_highlights = function(hl, _)
+			for group, colors in pairs(require('colorscheme').get_all_highlights()) do
+				hl[group] = colors
+			end
 		end,
 	})
 	vim.cmd([[colorscheme tokyonight]])
