e7074814ae47478e3b6abf19e8f15412e96c795a
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-01 02:35:42 -0300

    feat(init): add vim_dir global variable

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
-require("options")
-require("utils")
-require("autocommands")
-require("lsp").setup()
-require("plugins")
-require("keymappings")
+vim.g.vim_dir = '~/.config/nvim'
+
+--  ╾──────────────────────────────────────────────────────────────────────────────────────────────────╼
+
+require('options')
+require('utils')
+require('autocommands')
+require('lsp').setup()
+require('plugins')
+require('keymappings')
