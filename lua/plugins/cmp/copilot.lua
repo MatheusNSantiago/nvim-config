e7b0e647b1246a17cf58ec72218974a65564e6c8
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-03 22:04:23 -0300

    feat(copilot): add support for suggestions

--- lua/plugins/cmp/copilot.lua
+++ lua/plugins/cmp/copilot.lua
@@ -1,56 +1,50 @@
 local M = {}
 
 function M.setup()
-    require("copilot_cmp").setup({
-        formatters = {
-            label = require("copilot_cmp.format").format_label_text,
-            insert_text = require("copilot_cmp.format").remove_existing,
-            preview = require("copilot_cmp.format").deindent,
-        },
-    })
+	require('copilot_cmp').setup({
+		formatters = {
+			label = require('copilot_cmp.format').format_label_text,
+			insert_text = require('copilot_cmp.format').remove_existing,
+			preview = require('copilot_cmp.format').deindent,
+		},
+	})
 
-    require("copilot").setup({
-        panel = {
-            enabled = false,
-            auto_refresh = false,
-            keymap = {
-                jump_prev = "<M-[>",
-                jump_next = "<M-]>",
-                accept = "<CR>",
-                refresh = "gr",
-                open = "<M-CR>",
-            },
-            layout = {
-                position = "bottom", -- | top | left | right
-                ratio = 0.4,
-            },
-        },
-        suggestion = {
-            enabled = false,
-            auto_trigger = false,
-            debounce = 75,
-            keymap = {
-                -- accept = "<Tab>", --"<C-f>",
-                accept_word = false,
-                accept_line = false,
-                next = "<M-]>",
-                prev = "<M-[>",
-                jump_next = "<M-]>", --"<C-n>",
-                -- dismiss = "<ESC>",
-            },
-        },
-        filetypes = {
-            help = false,
-            -- gitcommit = false,
-            markdown = true,
-            gitrebase = false,
-            hgcommit = false,
-            svn = false,
-            cvs = false,
-            ["."] = false,
-        },
-        copilot_node_command = "node", -- Node.js version must be > 16.x
-    })
+	require('copilot').setup({
+		panel = {
+			enabled = false,
+			auto_refresh = false,
+			keymap = {
+				jump_prev = '<M-[>',
+				jump_next = '<M-]>',
+				accept = '<CR>',
+				refresh = 'gr',
+				open = '<M-CR>',
+			},
+			layout = {
+				position = 'bottom', -- | top | left | right
+				ratio = 0.4,
+			},
+		},
+		suggestion = {
+			enabled = true,
+			auto_trigger = false,
+			debounce = 75,
+			keymap = {
+				-- accept = '<S-ç>', --"<C-f>",
+				accept_word = false,
+				accept_line = false,
+				next = '<M-]>',
+				prev = '<M-[>',
+				-- jump_next = "<M-]>", --"<C-n>",
+				-- dismiss = "<ESC>",
+			},
+		},
+		filetypes = {
+			help = false,
+			['*'] = true, -- disable for all other filetypes and ignore default `filetypes`
+		},
+		copilot_node_command = 'node', -- Node.js version must be > 16.x
+	})
 end
 
 return M
--- lua/plugins/cmp/mappings.lua
+++ lua/plugins/cmp/mappings.lua
@@ -7,9 +7,14 @@ local function check_backspace()
 	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
-local function has_words_before()
-	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-		return false
+local function tab(fallback)
+	local copilot_ok, suggestion = pcall(require, 'copilot.suggestion')
+	if luasnip.jumpable() then
+		return luasnip.jump(1)
+	elseif copilot_ok and suggestion.is_visible() then
+		suggestion.accept()
+	else
+		fallback()
 	end
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
