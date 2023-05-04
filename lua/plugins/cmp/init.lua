53687405adabef0b67509e833517fc72012f9525
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-03 22:05:38 -0300

    feat(cmp): make so that completions dont expand args

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -1,59 +1,59 @@
 local M = {}
 function M.setup()
 	return {
-		"hrsh7th/nvim-cmp",
+		'hrsh7th/nvim-cmp',
 		config = M.config,
 	}
 end
 
 function M.config()
-	local cmp = require("cmp")
-	local types = require("cmp.types.cmp")
+	local cmp = require('cmp')
+	local types = require('cmp.types.cmp')
 
-	local lspkind = require("lspkind")
-	local luasnip = require("luasnip")
+	local lspkind = require('lspkind')
+	local luasnip = require('luasnip')
 
 	cmp.setup({
 		snippet = {
 			expand = function(args)
-				luasnip.lsp_expand(args.body)
+				local body = args.body:gsub('%b()', '(${1})') -- remove tudo entre os parenteses
+				luasnip.lsp_expand(body)
 			end,
 		},
-		mapping = require("plugins.cmp.mappings"),
-		-- completion = { completeopt = "menu,menuone,noinsert" },
+		mapping = require('plugins.cmp.mappings'),
 		sources = cmp.config.sources({
 			{
-				name = "luasnip",
-				priority = 7,
+				name = 'nvim_lsp',
+				priority = 1000,
+				-- max_item_count = 10,
+				entry_filter = require('plugins.cmp.utils.limit_lsp_types'),
+				group_index = 1,
+			},
+			{
+				name = 'copilot',
+				priority = 900,
+				keyword_length = 0,
+				group_index = 1,
+			},
+			{
+				name = 'luasnip',
+				priority = 750,
 				max_item_count = 5,
 				keyword_length = 2,
 				group_index = 1,
 			},
 			{
-				name = "nvim_lsp",
-				-- priority = 10,
-				-- max_item_count = 10,
-				-- entry_filter = require("plugins.cmp.utils.limit_completions"),
-				group_index = 1,
-			},
-			{
-				name = "path",
-				-- priority = 20,
-				max_item_count = 4,
-				group_index = 1,
-			},
-			{
-				name = "copilot",
-				-- priority = 9,
-				keyword_length = 0,
-				group_index = 1,
-			},
-			{
-				name = "buffer",
-				-- priority = 7,
+				name = 'buffer',
+				priority = 500,
 				keyword_length = 4,
 				max_item_count = 5,
 				group_index = 2,
+			},
+			{
+				name = 'path',
+				priority = 250,
+				max_item_count = 4,
+				group_index = 1,
 			},
 		}),
 		duplicates = {
@@ -63,36 +63,35 @@ function M.config()
 			luasnip = 0,
 		},
 		sorting = {
-			-- priority_weight = 2,
+			priority_weight = 2,
 			comparators = {
-				cmp.config.compare.kind,
+				require('copilot_cmp.comparators').prioritize,
+				-- require('plugins.cmp.utils.comparators').deprioritize_snippet,
+				require('cmp-under-comparator').under,
+				-- cmp.config.compare.kind,
 				cmp.config.compare.exact,
-				require("plugins.cmp.utils.comparators").deprioritize_snippet,
-				require("copilot_cmp.comparators").prioritize,
-				require("copilot_cmp.comparators").score,
-				cmp.config.compare.offset,
 				cmp.config.compare.score,
 				cmp.config.compare.locality,
-				require("cmp-under-comparator").under,
+				cmp.config.compare.offset,
 				cmp.config.compare.sort_text,
 				cmp.config.compare.order,
 			},
 		},
 		formatting = {
-			fields = { "kind", "abbr", "menu" },
+			fields = { 'kind', 'abbr', 'menu' },
 			format = lspkind.cmp_format({
-				mode = "symbol",
+				mode = 'symbol',
 				max_width = 50,
-				symbol_map = require("utils.icons").lspkind,
+				symbol_map = require('utils.icons').lspkind,
 				before = function(entry, vim_item)
 					vim_item.dup = ({ nvim_lsp = 0, path = 0 })[entry.source.name] or 0
 
-					if vim_item.kind == "Color" and entry.completion_item.documentation then
-						vim_item = require("plugins.cmp.utils.custom_formats").format_tailwind(entry, vim_item) -- for tailwind css autocomplete
+					if vim_item.kind == 'Color' and entry.completion_item.documentation then
+						vim_item = require('plugins.cmp.utils.custom_formats').format_tailwind(entry, vim_item) -- for tailwind css autocomplete
 					end
 
-					if vim.tbl_contains({ "path" }, entry.source.name) then
-						local icon, hl_group = require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
+					if vim.tbl_contains({ 'path' }, entry.source.name) then
+						local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
 						if icon then
 							vim_item.kind = icon
 							vim_item.kind_hl_group = hl_group
@@ -109,54 +108,52 @@ function M.config()
 		},
 		window = {
 			completion = cmp.config.window.bordered({
-				winhighlight = "Normal:TelescopeNormal,FloatBorder:TelescopeBorder",
-				border = "rounded",
+				winhighlight = 'Normal:TelescopeNormal,FloatBorder:TelescopeBorder',
+				border = 'rounded',
 				col_offset = -3,
 				side_padding = 1,
 			}),
 			documentation = cmp.config.window.bordered({
-				winhighlight = "Normal:TelescopeNormal,FloatBorder:TelescopeBorder",
+				winhighlight = 'Normal:TelescopeNormal,FloatBorder:TelescopeBorder',
 			}),
 		},
 		experimental = { ghost_text = true },
 		preselect = cmp.PreselectMode.Item,
-		view = {
-			entries = "custom", -- can be "custom", "wildmenu" or "native"
-		},
+		view = { entries = 'custom' }, -- can be "custom", "wildmenu" or "native"
 	})
 
 	-- Set configuration for specific filetype.
-	cmp.setup.filetype("gitcommit", {
+	cmp.setup.filetype('gitcommit', {
 		sources = cmp.config.sources({
-			{ name = "conventionalcommits" },
-		}, { { name = "buffer" } }),
+			{ name = 'conventionalcommits' },
+		}, { { name = 'buffer' } }),
 	})
 
-	require("plugins.cmp.copilot").setup()
-	require("plugins.cmp.cmdline").setup()
+	require('plugins.cmp.copilot').setup()
+	require('plugins.cmp.cmdline').setup()
 end
 
-local c = require("utils.colors")
+local c = require('utils.colors')
 M.highlights = {
-	CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE", strikethrough = true },
-	CmpItemAbbrMatch = { fg = c.secondary, bg = "NONE", bold = true },
-	CmpItemAbbrMatchFuzzy = { fg = c.secondary, bg = "NONE", bold = true },
-	CmpItemMenu = { fg = "#C792EA", bg = "NONE", italic = true },
-	CmpItemKindColor = { fg = "#D8EEEB", bg = "None" },
-	CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "None" },
-	CmpItemAbbr = { fg = "White", bg = "NONE" },
-	CmpItemKindText = { fg = "LightGrey" },
-	CmpItemKindFunction = { fg = "#C586C0" },
-	CmpItemKindClass = { fg = "Orange" },
-	CmpItemKindKeyword = { fg = "#f90c71" },
-	CmpItemKindSnippet = { fg = "#565c64" },
-	CmpItemKindConstructor = { fg = "#ae43f0" },
-	CmpItemKindVariable = { fg = "#9CDCFE", bg = "NONE" },
-	CmpItemKindInterface = { fg = "#f90c71", bg = "NONE" },
-	CmpItemKindFolder = { fg = "#2986cc" },
-	CmpItemKindReference = { fg = "#922b21" },
-	CmpItemKindMethod = { fg = "#C586C0" },
-	CmpItemKindCopilot = { fg = "#6CC644" },
+	CmpItemAbbrDeprecated = { fg = '#7E8294', bg = 'NONE', strikethrough = true },
+	CmpItemAbbrMatch = { fg = c.secondary, bg = 'NONE', bold = true },
+	CmpItemAbbrMatchFuzzy = { fg = c.secondary, bg = 'NONE', bold = true },
+	CmpItemMenu = { fg = '#C792EA', bg = 'NONE', italic = true },
+	CmpItemKindColor = { fg = '#D8EEEB', bg = 'None' },
+	CmpItemKindTypeParameter = { fg = '#D8EEEB', bg = 'None' },
+	CmpItemAbbr = { fg = 'White', bg = 'NONE' },
+	CmpItemKindText = { fg = 'LightGrey' },
+	CmpItemKindFunction = { fg = '#C586C0' },
+	CmpItemKindClass = { fg = 'Orange' },
+	CmpItemKindKeyword = { fg = '#f90c71' },
+	CmpItemKindSnippet = { fg = '#565c64' },
+	CmpItemKindConstructor = { fg = '#ae43f0' },
+	CmpItemKindVariable = { fg = '#9CDCFE', bg = 'NONE' },
+	CmpItemKindInterface = { fg = '#f90c71', bg = 'NONE' },
+	CmpItemKindFolder = { fg = '#2986cc' },
+	CmpItemKindReference = { fg = '#922b21' },
+	CmpItemKindMethod = { fg = '#C586C0' },
+	CmpItemKindCopilot = { fg = '#6CC644' },
 }
 
 return M
