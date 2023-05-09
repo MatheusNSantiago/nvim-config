56cf02fe653371b6ca740fcb74d5786ba8995dc4
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-08 22:12:27 -0300

    fix(lsp_signature): fix parameter higlight

--- lua/plugins/lsp/lsp_signature.lua
+++ lua/plugins/lsp/lsp_signature.lua
@@ -14,13 +14,13 @@ end
 
 function M.config()
 	require('lsp_signature').setup({
-		bind = false,                    -- This is mandatory, otherwise border config won't get registered. If you want to hook lspsaga or other signature handler, pls set to false
+		bind = true,                    -- This is mandatory, otherwise border config won't get registered. If you want to hook lspsaga or other signature handler, pls set to false
 		doc_lines = 10,                  -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
 		max_height = 12,                 -- max height of signature floating_window
 		max_width = 80,                  -- max_width of signature floating_window
-		noice = true,                    -- set to true if you using noice to render markdown
+		-- noice = true,                    -- set to true if you using noice to render markdown
 		wrap = true,                     -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
-		floating_window = false,         -- show hint in a floating window, set to false for virtual text only mode
+		floating_window = true,          -- show hint in a floating window, set to false for virtual text only mode
 		floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
 		-- will set to true when fully tested, set to false will use whichever side has more space
 		-- this setting will be helpful if you do not want the PUM and floating win overlap
@@ -34,6 +34,7 @@ function M.config()
 		hint_prefix = '🐼 ',                  -- Panda for parameter, NOTE => for the terminal not support emoji, might crash
 		hint_scheme = 'String',
 		hi_parameter = 'LspSignatureActiveParameter', -- how your parameter will be highlight
+		-- hi_parameter = 'IncSearch', -- how your parameter will be highlight
 		handler_opts = { border = 'rounded' },  -- double, rounded, single, shadow, none, or a table of borders ,
 		always_trigger = false,                 -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
 		auto_close_after = nil,                 -- autoclose signature float win after x sec, disabled if nil.
@@ -48,5 +49,10 @@ function M.config()
 		move_cursor_key = nil,                  -- imap, use nvim_set_current_win to move cursor between current win and floating
 	})
 end
+
+local c= require('utils.colors')
+M.highlights = {
+	LspSignatureActiveParameter = { fg = c.secondary, bold = true, underline = true },
+}
 
 return M
