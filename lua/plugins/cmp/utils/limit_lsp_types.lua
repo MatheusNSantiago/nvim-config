c19673715d7bf71e010fc58cc0784e6697769c07
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-08 21:50:49 -0300

    feat(lsp): add new limit completions

--- lua/plugins/cmp/utils/limit_lsp_types.lua
+++ lua/plugins/cmp/utils/limit_lsp_types.lua
@@ -1,31 +1,33 @@
 -- Limits LSP results to specific types based on line context (FIelds, Methods, Variables)
 return function(entry, ctx)
-	local types = require("cmp.types")
+	local deu_certo, res = pcall(function()
+		local kind = require('cmp.types.lsp').CompletionItemKind[entry:get_kind()]
 
-	local kind = types.lsp.CompletionItemKind
-	local entry_kind = entry:get_kind()
-	local col = vim.api.nvim_win_get_cursor(0)[2]
-	local line = vim.api.nvim_get_current_line()
+		local col = vim.api.nvim_win_get_cursor(0)[2]
+		local line = vim.api.nvim_win_get_cursor(0)[1]
+		local prev_line = vim.api.nvim_buf_get_lines(0, line - 2, line - 1, false)[1]
+		local cur_line = vim.api.nvim_get_current_line()
 
-	local char_before = line:sub(col, col)
-	local char_after = line:sub(col + 1, col + 1)
+		local char_before = cur_line:sub(col - 1, col - 1)
+		local char = cur_line:sub(col, col)
+		local char_after = cur_line:sub(col + 1, col + 1)
 
-	-- Evita que mostre autocomplete no final de funções do python
-	if char_before == ":" and char_after == "" then
-		return false
+		local prev_line_last_char = prev_line:sub(-1, -1)
+
+		-- Evita que mostre autocomplete no final de funções do python
+		if char == ':' and char_after == '' then return false end
+
+		if char == '.' then return (kind ~= 'Snippet') end
+
+		if char_before == '\t' and prev_line_last_char == '(' or prev_line_last_char == ',' then
+			return (kind ~= 'Snippet')
+		end
+
+		return true
+	end)
+	if deu_certo then
+		return res
+	else
+		return true
 	end
-
-	if char_before == ":" and char_after:match("[a-zA-Z]") then
-		return entry_kind == kind.Function
-	end
-
-	if char_before == "." and char_after:match("[a-zA-Z]") then
-		return (entry_kind == kind.Method) or (entry_kind == kind.Field) or (entry_kind == kind.Property)
-	end
-
-	if string.match(line, "^%s+%w+$") then
-		return entry_kind == kind.Function or entry_kind == kind.Variable
-	end
-
-	return true
 end
