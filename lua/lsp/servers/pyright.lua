23ea12a51962f21dcbd9d778e0799308e7e68b53
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-14 15:55:15 -0300

    feat(pyright): make vanila diagnostic invisible

--- lua/lsp/servers/pyright.lua
+++ lua/lsp/servers/pyright.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+M.handlers = {
+	["textDocument/publishDiagnostics"] = vim.lsp.with(
+		vim.lsp.diagnostic.on_publish_diagnostics,
+		{ virtual_text = false }
+	),
+}
+
+
 M.settings = {
 	pyright = { autoImportCompletion = true },
 	python = {
