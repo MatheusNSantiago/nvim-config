a7b7f747522a26906517a744c2681b997aa290c3
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-14 15:54:41 -0300

    feat(pyright): add keymaps specific to python

--- lua/lsp/servers/pyright.lua
+++ lua/lsp/servers/pyright.lua
@@ -1,16 +1,22 @@
 local M = {}
 
 M.settings = {
-  pyright = { autoImportCompletion = true },
-  python = {
-    analysis = {
-      autoSearchPaths = true,
-      diagnosticMode = "openFilesOnly",
-      useLibraryCodeForTypes = true,
-      completeFunctionParens = true,
-      typeCheckingMode = "off",
-    },
-  },
+	pyright = { autoImportCompletion = true },
+	python = {
+		analysis = {
+			autoSearchPaths = true,
+			diagnosticMode = 'openFilesOnly',
+			useLibraryCodeForTypes = true,
+			completeFunctionParens = true,
+			typeCheckingMode = 'off',
+		},
+	},
 }
+
+M.on_attach = function(client, bufnr)
+	require('lsp').common_on_attach(client, bufnr)
+
+	utils.api.keymap('n', '<leader>r', [[:w<CR>:TermExec cmd='python "%"'<CR>]], { buffer = bufnr, desc = 'python: [R]un' })
+end
 
 return M
