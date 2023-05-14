ed01988e745d7599399ac613f1725fa8a5b56d46
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-14 11:32:48 -0300

    fix(lualine): make lsp display work only in lsps

--- lua/plugins/ui/lualine/statusline.lua
+++ lua/plugins/ui/lualine/statusline.lua
@@ -1,71 +1,63 @@
-local icons = require("utils.icons")
-local c = vim.tbl_extend("force", require("utils.colors"), {
-	normal = "#7e9cd8",
-	insert = "#98bb6c",
-	visual = "#ffa066",
-	replace = "#e46876",
-	command = "#e6c384",
+local icons = require('utils.icons')
+local c = vim.tbl_extend('force', require('utils.colors'), {
+	normal = '#7e9cd8',
+	insert = '#98bb6c',
+	visual = '#ffa066',
+	replace = '#e46876',
+	command = '#e6c384',
 })
 
 local conditions = {
-	buffer_not_empty = function()
-		return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
-	end,
-	hide_in_width = function()
-		return vim.fn.winwidth(0) > 80
-	end,
+	buffer_not_empty = function() return vim.fn.empty(vim.fn.expand('%:t')) ~= 1 end,
+	hide_in_width = function() return vim.fn.winwidth(0) > 80 end,
 	check_git_workspace = function()
-		local filepath = vim.fn.expand("%:p:h")
-		local gitdir = vim.fn.finddir(".git", filepath .. ";")
+		local filepath = vim.fn.expand('%:p:h')
+		local gitdir = vim.fn.finddir('.git', filepath .. ';')
 		return gitdir and #gitdir > 0 and #gitdir < #filepath
 	end,
 }
 
 local function show_macro_recording()
 	local rec_reg = vim.fn.reg_recording()
-	if rec_reg == "" then
-		return ""
+	if rec_reg == '' then
+		return ''
 	else
-		return "recording @" .. rec_reg
+		return 'recording @' .. rec_reg
 	end
 end
 
 local function get_mode_color()
 	local mode = vim.fn.mode()
 
-	if mode == "n" then
-		return { fg = c["normal"] }
-	elseif mode == "i" then
-		return { fg = c["insert"] }
-	elseif mode == "v" or mode == "V" then
-		return { fg = c["visual"] }
-	elseif mode == "c" then
-		return { fg = c["command"] }
+	if mode == 'n' then
+		return { fg = c['normal'] }
+	elseif mode == 'i' then
+		return { fg = c['insert'] }
+	elseif mode == 'v' or mode == 'V' then
+		return { fg = c['visual'] }
+	elseif mode == 'c' then
+		return { fg = c['command'] }
 	end
 end
 
-local function left_separator()
-	return ""
-end
-local function right_separator()
-	return ""
-end
+local function left_separator() return '' end
+local function right_separator() return '' end
 
 local flutter = vim.g.flutter_tools_decorations or {}
 
 return {
 	lualine_a = {
-		{ "mode", color = { fg = c.dark_bg, gui = "bold" } },
+		{ 'mode', color = { fg = c.dark_bg, gui = 'bold' } },
 	},
 	lualine_b = {
 		{ left_separator, color = get_mode_color, padding = { right = 0, left = 0 } },
-		{ "FugitiveHead", icon = "" },
+		{ 'FugitiveHead', icon = '' },
 		{
-			"diff",
+			'diff',
 			symbols = {
-				added = icons.git.Add .. " ",
-				modified = icons.git.Mod .. " ",
-				removed = icons.git.Remove .. " ",
+				added = icons.git.Add .. ' ',
+				modified = icons.git.Mod .. ' ',
+				removed = icons.git.Remove .. ' ',
 			},
 			diff_color = {
 				added = { fg = c.green },
@@ -75,19 +67,19 @@ return {
 			cond = conditions.hide_in_width,
 		},
 		{
-			"macro-recording",
+			'macro-recording',
 			fmt = show_macro_recording,
 		},
 	},
 	lualine_c = {
 		{ left_separator, color = { fg = c.outerbg }, padding = { right = 0, left = 0 } },
-		{ "%=" },
+		{ '%=' },
 		{
-			"filename",
+			'filename',
 			cond = conditions.buffer_not_empty,
 			file_status = false,
 			path = 1,
-			color = { fg = c.magenta, gui = "bold" },
+			color = { fg = c.magenta, gui = 'bold' },
 		},
 	},
 	lualine_x = {
@@ -95,13 +87,13 @@ return {
 	},
 	lualine_y = {
 		{
-			"diagnostics",
-			sources = { "nvim_lsp", "nvim_diagnostic", "nvim_workspace_diagnostic" },
+			'diagnostics',
+			sources = { 'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic' },
 			symbols = {
-				error = icons.diagnostics.Error .. " ",
-				warn = icons.diagnostics.Warning .. " ",
-				info = icons.diagnostics.Information .. " ",
-				hint = icons.diagnostics.Hint .. " ",
+				error = icons.diagnostics.Error .. ' ',
+				warn = icons.diagnostics.Warning .. ' ',
+				info = icons.diagnostics.Information .. ' ',
+				hint = icons.diagnostics.Hint .. ' ',
 			},
 			diagnostics_color = {
 				color_error = { fg = c.red },
@@ -112,33 +104,32 @@ return {
 		{
 			-- Lsp server name .
 			function()
-				local msg = "No LSP"
-				local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+				local msg = 'No LSP'
+				local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
 				local clients = vim.lsp.get_active_clients()
-				if next(clients) == nil then
-					return msg
-				end
+				if next(clients) == nil then return msg end
+
 				for _, client in ipairs(clients) do
 					local filetypes = client.config.filetypes
 					if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-						return "[" .. client.name .. "]"
+						local servers = require('lsp').servers
+						if vim.tbl_contains(servers, client.name) then return client.name  end
 					end
 				end
+
 				return msg
 			end,
-			icon = "",
-			color = { fg = "#ffffff", gui = "bold" },
-			on_click = function()
-				vim.cmd([[LspInfo]])
-			end,
+			icon = '',
+			color = { fg = '#ffffff', gui = 'bold' },
+			on_click = function() vim.cmd([[LspInfo]]) end,
 		},
-		{ "searchcount",   maxcount = 999,         timeout = 500 },
+		{ 'searchcount',   maxcount = 999,         timeout = 500 },
 		{ right_separator, color = get_mode_color, padding = { right = 0, left = 0 } },
 	},
 	lualine_z = {
 		-- { { flutter.device or "" } },
 		{
-			"%p%%/%L",
+			'%p%%/%L',
 			cond = conditions.buffer_not_empty,
 			color = { fg = c.dark_bg },
 			padding = { right = 1, left = 1 },
