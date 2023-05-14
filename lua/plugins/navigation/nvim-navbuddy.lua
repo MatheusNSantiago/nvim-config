1e0d055d5d5fc829cb95f3704a5ae75ecc532bc6
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-05-14 16:20:42 -0300

    feat(nvim-navbuddy): fix mappings

--- lua/plugins/navigation/nvim-navbuddy.lua
+++ lua/plugins/navigation/nvim-navbuddy.lua
@@ -61,33 +61,32 @@ function M.config()
 			icons = { leaf = '  ', leaf_selected = ' → ', branch = ' ' },
 		},
 		icons = get_kinds_padded(),
-		use_default_mappings = true, -- If set to false, only mappings set
-		-- by user are set. Else default mappings are used for keys that are not set by user
+		use_default_mappings = false, -- If set to false, only mappings set
 		mappings = {
-			['<esc>'] = actions.close, -- Close and cursor to original location
-			['q'] = actions.close,
-			['k'] = actions.next_sibling, -- down
-			['l'] = actions.previous_sibling, -- up
-			['j'] = actions.parent,  -- Move to left panel
-			['ç'] = actions.children, -- Move to right panel
-			['0'] = actions.root,    -- Move to first panel
-			['v'] = actions.visual_name, -- Visual selection of name
-			['V'] = actions.visual_scope, -- Visual selection of scope
-			['y'] = actions.yank_name, -- Yank the name to system clipboard "+
-			['Y'] = actions.yank_scope, -- Yank the scope to system clipboard "+
-			['i'] = actions.insert_name, -- Insert at start of name
-			['I'] = actions.insert_scope, -- Insert at start of scope
-			['a'] = actions.append_name, -- Insert at end of name
-			['A'] = actions.append_scope, -- Insert at end of scope
-			['r'] = actions.rename,  -- Rename currently focused symbol
-			['d'] = actions.delete,  -- Delete scope
-			['f'] = actions.fold_create, -- Create fold of current scope
-			['F'] = actions.fold_delete, -- Delete fold of current scope
-			['c'] = actions.comment, -- Comment out current scope
-			['<enter>'] = actions.select, -- Goto selected symbol
-			['o'] = actions.select,
-			['<A-k>'] = actions.move_down, -- Move focused node down
-			['<A-l>'] = actions.move_up, -- Move focused node up
+			['<esc>'] = actions.close(), -- Close and cursor to original location
+			['q'] = actions.close(),
+			['k'] = actions.next_sibling(), -- down
+			['l'] = actions.previous_sibling(), -- up
+			['j'] = actions.parent(),  -- Move to left panel
+			['ç'] = actions.children(), -- Move to right panel
+			['0'] = actions.root(),    -- Move to first panel
+			['v'] = actions.visual_name(), -- Visual selection of name
+			['V'] = actions.visual_scope(), -- Visual selection of scope
+			['y'] = actions.yank_name(), -- Yank the name to system clipboard "+
+			['Y'] = actions.yank_scope(), -- Yank the scope to system clipboard "+
+			['i'] = actions.insert_name(), -- Insert at start of name
+			['I'] = actions.insert_scope(), -- Insert at start of scope
+			['a'] = actions.append_name(), -- Insert at end of name
+			['A'] = actions.append_scope(), -- Insert at end of scope
+			['r'] = actions.rename(),  -- Rename currently focused symbol
+			['d'] = actions.delete(),  -- Delete scope
+			['f'] = actions.fold_create(), -- Create fold of current scope
+			['F'] = actions.fold_delete(), -- Delete fold of current scope
+			['c'] = actions.comment(), -- Comment out current scope
+			['<enter>'] = actions.select(), -- Goto selected symbol
+			['o'] = actions.select(),
+			['<A-k>'] = actions.move_down(), -- Move focused node down
+			['<A-l>'] = actions.move_up(), -- Move focused node up
 			['t'] = actions.telescope({ -- Fuzzy finder at current level.
 				layout_config = {
 					-- All options that can be
@@ -99,16 +98,16 @@ function M.config()
 				layout_strategy = 'horizontal',
 			}),
 		},
-		lsp = {
-			auto_attach = false, -- If set to true, you don't need to manually use attach function
-			preference = nil, -- list of lsp server names in order of preference
-		},
-		source_buffer = {
-			follow_node = true, -- Keep the current node in focus on the source buffer
-			highlight = true, -- Highlight the currently focused node
-			reorient = 'smart', -- "smart", "top", "mid" or "none"
-			scrolloff = nil, -- scrolloff value when navbuddy is open
-		},
+		-- lsp = {
+		-- 	auto_attach = false, -- If set to true, you don't need to manually use attach function
+		-- 	preference = nil, -- list of lsp server names in order of preference
+		-- },
+		-- source_buffer = {
+		-- 	follow_node = true, -- Keep the current node in focus on the source buffer
+		-- 	highlight = true, -- Highlight the currently focused node
+		-- 	reorient = 'smart', -- "smart", "top", "mid" or "none"
+		-- 	scrolloff = nil, -- scrolloff value when navbuddy is open
+		-- },
 	})
 end
 
