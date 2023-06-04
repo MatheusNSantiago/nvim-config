fffaf669215f1600f060cfa2571928191dc411f5
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-06-04 10:07:43 -0300

    feat(flit): disable multiline hopping

--- lua/plugins/navigation/flit.lua
+++ lua/plugins/navigation/flit.lua
@@ -15,7 +15,7 @@ function M.config()
 		keys = { f = 'f', F = 'F', t = 't', T = 'T' },
 		-- A string like "nv", "nvo", "o", etc.
 		labeled_modes = 'v',
-		multiline = true,
+		multiline = false,
 		-- Like `leap`s similar argument (call-specific overrides).
 		-- E.g.: opts = { equivalence_classes = {} }
 		opts = {},
