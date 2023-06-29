bec33bf219e279fcfa062a06958c2cc2cc6c39c8
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-06-29 09:16:18 -0300

    feat(tssserver): change shift width to 2

--- lua/lsp/servers/tsserver.lua
+++ lua/lsp/servers/tsserver.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.preselectSupport = true
@@ -10,86 +10,92 @@ capabilities.textDocument.completion.completionItem.deprecatedSupport = true
 capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
 capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
 capabilities.textDocument.completion.completionItem.resolveSupport = {
-	properties = {
-		"documentation",
-		"detail",
-		"additionalTextEdits",
-	},
+    properties = {
+        'documentation',
+        'detail',
+        'additionalTextEdits',
+    },
 }
 capabilities.textDocument.codeAction = {
-	dynamicRegistration = false,
-	codeActionLiteralSupport = {
-		codeActionKind = {
-			valueSet = {
-				"",
-				"quickfix",
-				"refactor",
-				"refactor.extract",
-				"refactor.inline",
-				"refactor.rewrite",
-				"source",
-				"source.organizeImports",
-			},
-		},
-	},
+    dynamicRegistration = false,
+    codeActionLiteralSupport = {
+        codeActionKind = {
+            valueSet = {
+                '',
+                'quickfix',
+                'refactor',
+                'refactor.extract',
+                'refactor.inline',
+                'refactor.rewrite',
+                'source',
+                'source.organizeImports',
+            },
+        },
+    },
 }
 capabilities.textDocument.foldingRange = {
-	dynamicRegistration = false,
-	lineFoldingOnly = true,
+    dynamicRegistration = false,
+    lineFoldingOnly = true,
 }
 
 local function filter(arr, fn)
-	if type(arr) ~= "table" then
-		return arr
-	end
+    if type(arr) ~= 'table' then return arr end
 
-	local filtered = {}
-	for k, v in pairs(arr) do
-		if fn(v, k, arr) then
-			table.insert(filtered, v)
-		end
-	end
+    local filtered = {}
+    for k, v in pairs(arr) do
+        if fn(v, k, arr) then table.insert(filtered, v) end
+    end
 
-	return filtered
+    return filtered
 end
 
 local function filterReactDTS(value)
-	-- Depending on typescript version either uri or targetUri is returned
-	if value.uri then
-		return string.match(value.uri, "%.d.ts") == nil
-	elseif value.targetUri then
-		return string.match(value.targetUri, "%.d.ts") == nil
-	end
+    -- Depending on typescript version either uri or targetUri is returned
+    if value.uri then
+        return string.match(value.uri, '%.d.ts') == nil
+    elseif value.targetUri then
+        return string.match(value.targetUri, '%.d.ts') == nil
+    end
 end
 
 local handlers = {
-	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-	["textDocument/publishDiagnostics"] = vim.lsp.with(
-		vim.lsp.diagnostic.on_publish_diagnostics,
-		{ virtual_text = true }
-	),
-	["textDocument/definition"] = function(err, result, method, ...)
-		if vim.tbl_islist(result) and #result > 1 then
-			local filtered_result = filter(result, filterReactDTS)
-			return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
-		end
+    ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' }),
+    ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' }),
+    ['textDocument/publishDiagnostics'] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics,
+        { virtual_text = true }
+    ),
+    ['textDocument/definition'] = function(err, result, method, ...)
+        if vim.tbl_islist(result) and #result > 1 then
+            local filtered_result = filter(result, filterReactDTS)
+            return vim.lsp.handlers['textDocument/definition'](err, filtered_result, method, ...)
+        end
 
-		vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
-	end,
+        vim.lsp.handlers['textDocument/definition'](err, result, method, ...)
+    end,
 }
 
 local on_attach = function(client, bufnr)
-	local custom_picker = require('plugins.navigation.telescope.picker')
-	require("lsp").common_on_attach(client, bufnr)
+    local custom_picker = require('plugins.navigation.telescope.picker')
+    utils.ft_helpers.filetype_settings({
+        [{ 'tsx', 'jsx', 'ts', 'js' }] = {
+            bo = {
+                tabstop = 2, -- number of columns occupied by a tab character
+                shiftwidth = 2, -- width for autoindents
+                softtabstop = 2, -- see multiple spaces as tabstops
+            },
+        },
+    })
 
-	custom_picker.create_picker('JS/TS', '<leader><leader>o', {
-		{
-			name = 'start vite dev server',
-			handler = "npm run dev",
-		},
-	})
-	utils.api.keymap("n", "gd", ":TypescriptGoToSourceDefinition<CR>")
+    require('lsp').common_on_attach(client, bufnr)
+
+    custom_picker.create_picker('JS/TS', '<leader><leader>o', {
+        {
+            name = 'start vite dev server',
+            handler = 'npm run dev',
+        },
+    })
+    utils.api.keymap('n', 'gd', ':TypescriptGoToSourceDefinition<CR>')
 end
 
 M.capabilities = capabilities
