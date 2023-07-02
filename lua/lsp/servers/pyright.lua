41964c84ec7cfb5b32855379ca7d3dd71e96ea94
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-01 21:47:47 -0300

    feat(python): add venv and installer picker commands

--- lua/lsp/servers/pyright.lua
+++ lua/lsp/servers/pyright.lua
@@ -1,30 +1,46 @@
 local M = {}
 
 M.handlers = {
-	["textDocument/publishDiagnostics"] = vim.lsp.with(
-		vim.lsp.diagnostic.on_publish_diagnostics,
-		{ virtual_text = false }
-	),
+    ['textDocument/publishDiagnostics'] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics,
+        { virtual_text = false }
+    ),
 }
 
-
 M.settings = {
-	pyright = { autoImportCompletion = true },
-	python = {
-		analysis = {
-			autoSearchPaths = true,
-			diagnosticMode = 'openFilesOnly',
-			useLibraryCodeForTypes = true,
-			completeFunctionParens = true,
-			typeCheckingMode = 'off',
-		},
-	},
+    pyright = { autoImportCompletion = true },
+    python = {
+        analysis = {
+            autoSearchPaths = true,
+            diagnosticMode = 'openFilesOnly',
+            useLibraryCodeForTypes = true,
+            completeFunctionParens = true,
+            typeCheckingMode = 'off',
+        },
+    },
 }
 
 M.on_attach = function(client, bufnr)
-	require('lsp').common_on_attach(client, bufnr)
+    require('lsp').common_on_attach(client, bufnr)
 
-	utils.api.keymap('n', '<leader>r', [[:w<CR>:TermExec cmd='python "%"'<CR>]], { buffer = bufnr, desc = 'python: [R]un' })
+    local custom_picker = require('plugins.navigation.telescope.picker')
+    custom_picker.create_picker('Python', '<leader><leader>o', {
+        {
+            name = 'Criar venv',
+            handler = 'python -m venv --upgrade-deps .venv',
+        },
+        {
+            name = 'install requirements',
+            handler = 'pip install -r requirements.txt',
+        },
+    })
+
+    utils.api.keymap(
+        'n',
+        '<leader>r',
+        [[:w<CR>:TermExec cmd='python "%"'<CR>]],
+        { buffer = bufnr, desc = 'python: [R]un' }
+    )
 end
 
 return M
