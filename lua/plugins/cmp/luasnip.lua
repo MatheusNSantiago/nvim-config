12671a5de494a59f1ccda80ddefe53e4c6bff12e
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-10 14:37:20 -0300

    feat(cmp): add custom snippets

--- lua/plugins/cmp/luasnip.lua
+++ lua/plugins/cmp/luasnip.lua
@@ -2,25 +2,25 @@ local M = {}
 
 function M.setup()
     return {
-        "L3MON4D3/LuaSnip",
+        'L3MON4D3/LuaSnip',
         config = M.config,
-        run = "make install_jsregexp",
+        run = 'make install_jsregexp',
         requires = {
-            "rafamadriz/friendly-snippets", -- Set of preconfigured snippets for different languages.
+            'rafamadriz/friendly-snippets', -- Set of preconfigured snippets for different languages.
         },
     }
 end
 
 function M.config()
-    local luasnip = require("luasnip")
+    local luasnip = require('luasnip')
 
     -- forget the current snippet when leaving the insert mode. ref: https://github.com/L3MON4D3/LuaSnip/issues/656#issuecomment-1313310146
-    local unlinkgrp = vim.api.nvim_create_augroup("UnlinkSnippetOnModeChange", { clear = true })
+    local unlinkgrp = vim.api.nvim_create_augroup('UnlinkSnippetOnModeChange', { clear = true })
 
-    vim.api.nvim_create_autocmd("ModeChanged", {
+    vim.api.nvim_create_autocmd('ModeChanged', {
         group = unlinkgrp,
-        pattern = { "s:n", "i:*" },
-        desc = "Forget the current snippet when leaving the insert mode",
+        pattern = { 's:n', 'i:*' },
+        desc = 'Forget the current snippet when leaving the insert mode',
         callback = function(evt)
             if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
                 luasnip.unlink_current()
@@ -28,12 +28,14 @@ function M.config()
         end,
     })
 
-    luasnip.filetype_extend("typescriptreact", { "html", "typescript" })
-    luasnip.filetype_extend("javascriptreact", { "html", "javascript" })
+    luasnip.filetype_extend('typescriptreact', { 'html', 'typescript' })
+    luasnip.filetype_extend('javascriptreact', { 'html', 'javascript' })
 
-    require("luasnip.loaders.from_vscode").lazy_load()
+    require('luasnip.loaders.from_vscode').lazy_load()
+    require('luasnip.loaders.from_vscode').lazy_load({ paths = './snippets' })
+
     luasnip.config.set_config({
-        region_check_events = "CursorMoved",
+        region_check_events = 'CursorMoved',
     })
 end
 
