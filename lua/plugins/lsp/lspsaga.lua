324d72d5dca6ca8776c9de2fafb0c31c928795a0
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-15 18:08:50 -0300

    fix(lspsaga): revert to previous commit to fix finder

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -1,82 +1,83 @@
 local M = {}
 
 function M.setup()
-	return {
-		'glepnir/lspsaga.nvim',
-		config = M.config,
-	}
+    return {
+        'glepnir/lspsaga.nvim',
+        commit = '4f075452c466df263e69ae142f6659dcf9324bf6',
+        config = M.config,
+    }
 end
 
 function M.config()
-	require('lspsaga').setup({
-		scroll_preview = {
-			scroll_down = '<C-d>',
-			scroll_up = '<C-u>',
-		},
-		request_timeout = 3000,
-		ui = {
-			title = true,
-			-- Border type can be single, double, rounded, solid, shadow.
-			border = 'single',
-			winblend = 0,
-			expand = '',
-			collapse = '',
-			code_action = '💡',
-			incoming = ' ',
-			outgoing = ' ',
-			hover = ' ',
-			kind = {},
-		},
-		symbol_in_winbar = {
-			enable = false,
-			separator = ' ',
-			ignore_patterns = {},
-			hide_keyword = true,
-			show_file = true,
-			folder_level = 1,
-			respect_root = false,
-			color_mode = true,
-		},
-		rename = {
-			quit = '<C-c>',
-			exec = '<CR>',
-			mark = 'x',
-			confirm = '<CR>',
-			in_select = true,
-		},
-		lightbulb = {
-			enable = false,
-			enable_in_insert = true,
-			sign = false,
-			sign_priority = 40,
-			virtual_text = true,
-		},
-		code_action = {
-			num_shortcut = true,
-			show_server_name = false,
-			extend_gitsigns = false,
-			keys = {
-				-- string | table type
-				quit = 'q',
-				exec = '<CR>',
-			},
-		},
-		finder = {
-			--percentage
-			max_height = 0.5,
-			force_max_height = false,
-			keys = {
-				jump_to = 'p',
-				edit = { 'o', '<CR>' },
-				vsplit = 's',
-				split = 'i',
-				tabe = 't',
-				tabnew = 'r',
-				quit = { 'q', '<ESC>' },
-				close_in_preview = '<ESC>',
-			},
-		},
-	})
+    require('lspsaga').setup({
+        scroll_preview = {
+            scroll_down = '<C-d>',
+            scroll_up = '<C-u>',
+        },
+        request_timeout = 3000,
+        ui = {
+            title = true,
+            -- Border type can be single, double, rounded, solid, shadow.
+            border = 'single',
+            winblend = 0,
+            expand = '',
+            collapse = '',
+            code_action = '💡',
+            incoming = ' ',
+            outgoing = ' ',
+            hover = ' ',
+            kind = {},
+        },
+        symbol_in_winbar = {
+            enable = false,
+            separator = ' ',
+            ignore_patterns = {},
+            hide_keyword = true,
+            show_file = true,
+            folder_level = 1,
+            respect_root = false,
+            color_mode = true,
+        },
+        rename = {
+            quit = '<C-c>',
+            exec = '<CR>',
+            mark = 'x',
+            confirm = '<CR>',
+            in_select = true,
+        },
+        lightbulb = {
+            enable = false,
+            enable_in_insert = true,
+            sign = false,
+            sign_priority = 40,
+            virtual_text = true,
+        },
+        code_action = {
+            num_shortcut = true,
+            show_server_name = false,
+            extend_gitsigns = false,
+            keys = {
+                -- string | table type
+                quit = 'q',
+                exec = '<CR>',
+            },
+        },
+        finder = {
+            --percentage
+            max_height = 0.5,
+            force_max_height = false,
+            keys = {
+                jump_to = 'p',
+                edit = { 'o', '<CR>' },
+                vsplit = 's',
+                split = 'i',
+                tabe = 't',
+                tabnew = 'r',
+                quit = { 'q', '<ESC>' },
+                close_in_preview = '<ESC>',
+            },
+        },
+    })
 end
 
 return M
