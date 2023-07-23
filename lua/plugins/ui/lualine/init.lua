1ea37397b010883bdba69d7bc2f7f1a869099cde
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-23 12:42:00 -0300

    refactor(utils): remove unused functions

--- lua/plugins/ui/lualine/init.lua
+++ lua/plugins/ui/lualine/init.lua
@@ -1,66 +1,64 @@
 local M = {}
 
 function M.setup()
-	return {
-		"nvim-lualine/lualine.nvim",
-		event = "BufReadPre",
-		config = M.config,
-	}
+  return {
+    'nvim-lualine/lualine.nvim',
+    event = 'BufReadPre',
+    config = M.config,
+  }
 end
 
 function M.config()
-	local icons = require("utils.icons")
-	local lualine = require("lualine")
-	local c = require("utils.colors")
+  local icons = require('utils.icons')
+  local lualine = require('lualine')
+  local c = require('utils.colors')
 
-	local repo_name = require("utils").get_repo_name()
+  local repo_name = vim.fn.fnamemodify(vim.fn.trim(vim.fn.system('git rev-parse --show-toplevel')), ':t')
 
-	lualine.setup({
-		options = {
-			theme = require("plugins.ui.lualine.theme"),
-			component_separators = { left = "", right = "" },
-			section_separators = { left = "", right = "" },
-		},
-		globalstatus = true,
-		icons_enabled = true,
-		refresh = {
-			statusline = 1000, -- The refresh option sets minimum time that lualine tries
-			tabline = 1000, -- to maintain between refresh. It's not guarantied if situation
-			winbar = 100, -- arises that lualine needs to refresh itself before this time
-		},
-		disabled_filetypes = {
-			-- statusline = { "NvimTree", "toggleterm" },
-			-- winbar = { "help", "startify", "packer", "neogitstatus", "Trouble", "Outline" },
-		},
-		sections = require("plugins.ui.lualine.statusline"),
-		-- inactive_sections = require("plugins.ui.lualine.statusline").sections,
-		winbar = {},
-		inactive_sections = {
-			lualine_x = {},
-			lualine_c = { { "%=" }, { "filename" } },
-		},
-		extensions = {
-			{
-				filetypes = { "toggleterm" },
-				sections = {},
-			},
-			"quickfix",
-			{
-				filetypes = { "NvimTree" },
-				sections = {},
-				winbar = {
-					lualine_b = {
-						{
-							function()
-								return icons.git.Git .. " " .. repo_name
-							end,
-							color = { bg = "None", fg = c.cyan, gui = "underline,bold" },
-						},
-					},
-				},
-			},
-		},
-	})
+  lualine.setup({
+    options = {
+      theme = require('plugins.ui.lualine.theme'),
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+    },
+    globalstatus = true,
+    icons_enabled = true,
+    refresh = {
+      statusline = 1000, -- The refresh option sets minimum time that lualine tries
+      tabline = 1000, -- to maintain between refresh. It's not guarantied if situation
+      winbar = 100,   -- arises that lualine needs to refresh itself before this time
+    },
+    disabled_filetypes = {
+      -- statusline = { "NvimTree", "toggleterm" },
+      -- winbar = { "help", "startify", "packer", "neogitstatus", "Trouble", "Outline" },
+    },
+    sections = require('plugins.ui.lualine.statusline'),
+    -- inactive_sections = require("plugins.ui.lualine.statusline").sections,
+    winbar = {},
+    inactive_sections = {
+      lualine_x = {},
+      lualine_c = { { '%=' }, { 'filename' } },
+    },
+    extensions = {
+      {
+        filetypes = { 'toggleterm' },
+        sections = {},
+      },
+      'quickfix',
+      {
+        filetypes = { 'NvimTree' },
+        sections = {},
+        winbar = {
+          lualine_b = {
+            {
+              function() return icons.git.Git .. ' ' .. repo_name end,
+              color = { bg = 'None', fg = c.cyan, gui = 'underline,bold' },
+            },
+          },
+        },
+      },
+    },
+  })
 end
 
 return M
--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -2,39 +2,32 @@ local M = {}
 
 -- return all tables flattened into one
 function M.flatten(tbl)
-	local result = {}
-	for _, sub_table in pairs(tbl) do
-		if type(sub_table) == 'table' then
-			for k, v in pairs(sub_table) do
-				result[k] = v
-			end
-		end
-	end
-	return result
+    local result = {}
+    for _, sub_table in pairs(tbl) do
+        if type(sub_table) == 'table' then
+            for k, v in pairs(sub_table) do
+                result[k] = v
+            end
+        end
+    end
+    return result
 end
 
 function M.set_hls(highlights)
-	for group, hl in pairs(highlights) do
-		vim.api.nvim_set_hl(0, group, hl)
-	end
+    for group, hl in pairs(highlights) do
+        vim.api.nvim_set_hl(0, group, hl)
+    end
 end
 
-function M.get_repo_name()
-	local full_path = vim.fn.trim(vim.fn.system('git rev-parse --show-toplevel'))
-	local repo_name = vim.fn.fnamemodify(full_path, ':t')
-	return repo_name
-end
 
 function M.log(msg, hl, name)
-	name = name or 'Neovim'
-	hl = hl or 'Todo'
-	vim.api.nvim_echo({ { name .. ': ', hl }, { msg } }, true, {})
+    name = name or 'Neovim'
+    hl = hl or 'Todo'
+    vim.api.nvim_echo({ { name .. ': ', hl }, { msg } }, true, {})
 end
 
 function M.warn(msg, name) vim.notify(msg, vim.log.levels.WARN, { title = name }) end
-
 function M.error(msg, name) vim.notify(msg, vim.log.levels.ERROR, { title = name }) end
-
 function M.info(msg, name) vim.notify(msg, vim.log.levels.INFO, { title = name }) end
 
 function M.isempty(s) return s == nil or s == '' end
@@ -43,22 +36,22 @@ function M.isempty(s) return s == nil or s == '' end
 ---@param item any
 ---@return boolean?
 function M.falsy(item)
-	if not item then return true end
-	local item_type = type(item)
-	if item_type == 'boolean' then return not item end
-	if item_type == 'string' then return item == '' end
-	if item_type == 'number' then return item <= 0 end
-	if item_type == 'table' then return vim.tbl_isempty(item) end
-	return item ~= nil
+    if not item then return true end
+    local item_type = type(item)
+    if item_type == 'boolean' then return not item end
+    if item_type == 'string' then return item == '' end
+    if item_type == 'number' then return item <= 0 end
+    if item_type == 'table' then return vim.tbl_isempty(item) end
+    return item ~= nil
 end
 
 function M.get_buf_option(opt)
-	local status_ok, buf_option = pcall(vim.api.nvim_buf_get_option, 0, opt)
-	if not status_ok then
-		return nil
-	else
-		return buf_option
-	end
+    local status_ok, buf_option = pcall(vim.api.nvim_buf_get_option, 0, opt)
+    if not status_ok then
+        return nil
+    else
+        return buf_option
+    end
 end
 
 --- Convert a list or map of items into a value by iterating all it's fields and transforming
@@ -69,21 +62,21 @@ end
 ---@param accum S?
 ---@return S
 function M.fold(callback, list, accum)
-	accum = accum or {}
-	for k, v in pairs(list) do
-		accum = callback(accum, v, k)
-		assert(accum ~= nil, 'The accumulator must be returned on each iteration')
-	end
-	return accum
+    accum = accum or {}
+    for k, v in pairs(list) do
+        accum = callback(accum, v, k)
+        assert(accum ~= nil, 'The accumulator must be returned on each iteration')
+    end
+    return accum
 end
 
 ---@generic T:table
 ---@param callback fun(item: T, key: any)
 ---@param list table<any, T>
 function M.foreach(callback, list)
-	for k, v in pairs(list) do
-		callback(v, k)
-	end
+    for k, v in pairs(list) do
+        callback(v, k)
+    end
 end
 
 ---@generic T
@@ -91,10 +84,10 @@ end
 ---@param list T[]
 ---@return T[]
 function M.map(callback, list)
-	return M.fold(function(accum, v, k)
-		accum[#accum + 1] = callback(v, k, accum)
-		return accum
-	end, list, {})
+    return M.fold(function(accum, v, k)
+        accum[#accum + 1] = callback(v, k, accum)
+        return accum
+    end, list, {})
 end
 
 --- Call the given function and use `vim.notify` to notify of any errors
@@ -106,22 +99,24 @@ end
 ---@return boolean, any
 ---@overload fun(func: function, ...): boolean, any
 function M.pcall(msg, func, ...)
-	local args = { ... }
-	if type(msg) == 'function' then
-		local arg = func --[[@as any]]
-		---@diagnostic disable-next-line: cast-local-type
-		args, func, msg = { arg, unpack(args) }, msg, nil
-	end
-	return xpcall(func, function(err)
-		msg = debug.traceback(msg and string.format('%s\n\n%s', msg, err) or err)
-		vim.schedule(function() vim.notify(msg, vim.log.levels.ERROR, { title = 'ERROR' }) end)
-	end, unpack(args))
+    local args = { ... }
+    if type(msg) == 'function' then
+        local arg = func --[[@as any]]
+        ---@diagnostic disable-next-line: cast-local-type
+        args, func, msg = { arg, unpack(args) }, msg, nil
+    end
+    return xpcall(func, function(err)
+        msg = debug.traceback(msg and string.format('%s\n\n%s', msg, err) or err)
+        vim.schedule(function() vim.notify(msg, vim.log.levels.ERROR, { title = 'ERROR' }) end)
+    end, unpack(args))
 end
+
 
 _G.utils = M
 _G.utils.api = require('utils.api-wrappers')
 _G.utils.icons = require('utils.icons')
 _G.utils.c = require('utils.colors')
 _G.utils.ft_helpers = require('utils.filetype-helpers')
+_G.create_picker = require('plugins.navigation.telescope.picker')
 
 return M
