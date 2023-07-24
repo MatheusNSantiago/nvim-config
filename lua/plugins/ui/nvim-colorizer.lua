71b75dc937156ed4420d1e92a2009ed8695b72e4
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-24 16:18:36 -0300

    feat(colorizer): add support for other types of formats

--- lua/plugins/ui/nvim-colorizer.lua
+++ lua/plugins/ui/nvim-colorizer.lua
@@ -4,11 +4,13 @@ function M.setup()
 	local is_installed = pcall(require, 'colorizer')
 
 	if is_installed then
+		---@diagnostic disable-next-line: missing-fields
 		utils.api.augroup('AttatchColorizer', {
-			event = { 'BufEnter', 'BufWritePost' },
+			event = { 'BufEnter', 'BufWritePost', 'BufReadPost' },
 			pattern = '*',
 			command = function()
 				if vim.bo.filetype == 'NvimTree' then return end
+
 				return vim.cmd([[ColorizerAttachToBuffer]])
 			end,
 		})
@@ -22,11 +24,21 @@ end
 
 function M.config()
 	require('colorizer').setup({
-		filetypes = { '*', '!NvimTree' },
+		buftype = { '*', '!prompt', '!nofile'},
+		filetypes = { '*', '!NvimTree', '!gitcommit' },
 		user_default_options = {
-			names = false,
 			tailwind = 'both',
-			mode = 'background',
+			names = false, -- "Name" codes like Blue or blue
+			RGB = false, -- #RGB hex codes
+			RRGGBB = true, -- #RRGGBB hex codes
+			RRGGBBAA = true, -- #RRGGBBAA hex codes
+			AARRGGBB = true, -- 0xAARRGGBB hex codes
+			rgb_fn = true, -- CSS rgb() and rgba() functions
+			hsl_fn = true, -- CSS hsl() and hsla() functions
+			-- css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+			css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			sass = { enable = false, parsers = { 'css' } }, -- Enable sass colors
+			mode = 'background', -- foreground, background,  virtualtext
 		},
 	})
 end
