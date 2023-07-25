67d48eab25a2c73c356dc4c8d864bbdd06d4f5eb
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-24 22:26:57 -0300

    feat(plugins): add kitty-navigator plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,9 +25,9 @@ return require('packer').startup(function(use)
 	--  │                          Base                            │
 	--  ╰──────────────────────────────────────────────────────────╯
 
-	use('wbthomason/packer.nvim') -- Package Manager
+	use('wbthomason/packer.nvim')  -- Package Manager
 	use('lewis6991/impatient.nvim') -- performance
-	use('nvim-lua/plenary.nvim') -- Common utilities
+	use('nvim-lua/plenary.nvim')   -- Common utilities
 
 	--  ╭──────────────────────────────────────────────────────────╮
 	--  │                        Interface                         │
@@ -35,22 +35,22 @@ return require('packer').startup(function(use)
 
 	setup('ui.tokyonight') -- Tema
 	setup('ui.nvim-tree') -- File Explorer
-	setup('ui.lualine') -- Status Line
+	setup('ui.lualine')   -- Status Line
 	setup('ui.indent-blankline')
 	setup('ui.web-devicons')
 	setup('ui.toggleterm') -- Terminal
 	setup('ui.bufferline') -- Tabs/buffers
-	setup('ui.dressing') -- selection e input
-	setup('ui.noice', { -- messages, cmdline and popupmenu
+	setup('ui.dressing')  -- selection e input
+	setup('ui.noice', {   -- messages, cmdline and popupmenu
 		'MunifTanjim/nui.nvim',
 	})
-	setup('ui.nvim-navic') -- 	breadcrumbs
-	setup('ui.nvim-notify') -- messages, cmdline and popupmenu
-	setup('ui.nvim-colorizer') -- #FFF ficar com o background branco
-	setup('ui.smart-splits') -- split pane management
-	setup('ui.vim-scrollbar') -- scrollbar
-	setup('ui.todo-comments') -- highlight TODO, FIXME, etc...
-	setup('ui.hlsearch') -- auto remove search highlight and rehighlight
+	setup('ui.nvim-navic')         -- 	breadcrumbs
+	setup('ui.nvim-notify')        -- messages, cmdline and popupmenu
+	setup('ui.nvim-colorizer')     -- #FFF ficar com o background branco
+	setup('ui.smart-splits')       -- split pane management
+	setup('ui.vim-scrollbar')      -- scrollbar
+	setup('ui.todo-comments')      -- highlight TODO, FIXME, etc...
+	setup('ui.hlsearch')           -- auto remove search highlight and rehighlight
 	setup('ui.rainbow-delimiters') -- rainbow parentheses
 
 	use('sitiom/nvim-numbertoggle') -- automatic relative/absolute line numbers
@@ -60,21 +60,22 @@ return require('packer').startup(function(use)
 	--  ╰──────────────────────────────────────────────────────────╯
 
 	setup('navigation.telescope', {
-		'nvim-telescope/telescope-media-files.nvim', -- mostra arquivos de mídia
-		'danielfalk/smart-open.nvim', --  melhora o ranking do find files
-		'kkharji/sqlite.lua', -- dependencia do smart-open
+		'nvim-telescope/telescope-media-files.nvim',                -- mostra arquivos de mídia
+		'danielfalk/smart-open.nvim',                               --  melhora o ranking do find files
+		'kkharji/sqlite.lua',                                       -- dependencia do smart-open
 		{ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }, -- melhora a performance
 	})
-	setup('navigation.hop') -- tipo o easymotion
-	setup('navigation.tabout') -- tabout
-	setup('navigation.marks') -- marks
-	setup('navigation.nvim-navbuddy') -- outline
-	setup('navigation.readline') -- Readline motions and deletions (igual do terminal)
-	setup('navigation.leap') -- sneap + easymotion
-	setup('navigation.flit') -- Repeat jump by pressing f, F, t, T again
-	setup('navigation.vim-matchup') -- highlight, navigate, and operate on %
+	setup('navigation.hop')                                       -- tipo o easymotion
+	setup('navigation.tabout')                                    -- tabout
+	setup('navigation.marks')                                     -- marks
+	setup('navigation.nvim-navbuddy')                             -- outline
+	setup('navigation.readline')                                  -- Readline motions and deletions (igual do terminal)
+	setup('navigation.leap')                                      -- sneap + easymotion
+	setup('navigation.flit')                                      -- Repeat jump by pressing f, F, t, T again
+	setup('navigation.vim-matchup')                               -- highlight, navigate, and operate on %
 
-	use('itchyny/vim-highlighturl') -- highlight URLs
+	use('itchyny/vim-highlighturl')                               -- highlight URLs
+	use('knubie/vim-kitty-navigator')                             -- navegação entre vim e kitty splits
 
 	--  ╭──────────────────────────────────────────────────────────╮
 	--  │                          Edição                          │
@@ -82,45 +83,45 @@ return require('packer').startup(function(use)
 
 	setup('edit.vim-autopairs') -- Completar (), [], {}
 	setup('edit.stay-in-place') -- mantém a posição do mouse ao indentar
-	setup('edit.comment') -- "gcc" to comment visual regions/lines
-	setup('edit.comment-box') -- comment box
+	setup('edit.comment')      -- "gcc" to comment visual regions/lines
+	setup('edit.comment-box')  -- comment box
 	setup('edit.prettier')
-	setup('edit.nvim-ufo', { -- folding
+	setup('edit.nvim-ufo', {   -- folding
 		'kevinhwang91/promise-async',
 	})
-	setup('edit.template-string') -- muda pra template-string automaticamente
-	setup('edit.vim-visual-multi') -- multicursor
-	setup('edit.neogen') -- documentation generation
-	setup('edit.undotree') -- undo tree
+	setup('edit.template-string')    -- muda pra template-string automaticamente
+	setup('edit.vim-visual-multi')   -- multicursor
+	setup('edit.neogen')             -- documentation generation
+	setup('edit.undotree')           -- undo tree
 	setup('edit.vim-textobj-comment') -- comment text objects
-	setup('edit.mini-move') -- Move lines and selections
-	setup('edit.nvim-neoclip') -- clipboard manager
-	setup('edit.leap-spooky') -- operar em text objects a distância
-	setup('edit.guess-indent') -- automatically detect the indentation settings
+	setup('edit.mini-move')          -- Move lines and selections
+	setup('edit.nvim-neoclip')       -- clipboard manager
+	setup('edit.leap-spooky')        -- operar em text objects a distância
+	setup('edit.guess-indent')       -- automatically detect the indentation settings
 
-	use('tpope/vim-surround') -- cs"'
-	use('tpope/vim-repeat') -- deixa o vim-surrond usar o '.'
-	use('wellle/targets.vim') -- adiciona novos textobjects
-	use('AndrewRadev/undoquit.vim') -- restaurar tabs fechadas
+	use('tpope/vim-surround')        -- cs"'
+	use('tpope/vim-repeat')          -- deixa o vim-surrond usar o '.'
+	use('wellle/targets.vim')        -- adiciona novos textobjects
+	use('AndrewRadev/undoquit.vim')  -- restaurar tabs fechadas
 
 	--  ╭──────────────────────────────────────────────────────────╮
 	--  │                           LSP                            │
 	--  ╰──────────────────────────────────────────────────────────╯
 
 	setup('lsp.mason', {
-		'neovim/nvim-lspconfig', -- LSP
+		'neovim/nvim-lspconfig',           -- LSP
 		'williamboman/mason-lspconfig.nvim', -- integrar mason com lspconfig
-		'jay-babu/mason-null-ls.nvim', -- deixa mais fácil usar mason + null-ls
+		'jay-babu/mason-null-ls.nvim',     -- deixa mais fácil usar mason + null-ls
 	})
 
 	setup('lsp.lsp_signature') -- mostra a function signature enquanto digita
-	setup('lsp.null-ls') -- Diagnostics/Formating/Code Actions
-	setup('lsp.lspsaga') -- LSP UIs
-	setup('lsp.illuminate') -- higlight a palavra em cima do cursor
-	setup('lsp.lsp-colors') -- agrupa os erros do LSP por cor
+	setup('lsp.null-ls')       -- Diagnostics/Formating/Code Actions
+	setup('lsp.lspsaga')       -- LSP UIs
+	setup('lsp.illuminate')    -- higlight a palavra em cima do cursor
+	setup('lsp.lsp-colors')    -- agrupa os erros do LSP por cor
 
 	use('b0o/schemastore.nvim') -- schemas para json
-	use('folke/neodev.nvim') -- docs for nvim Lua API
+	use('folke/neodev.nvim')   -- docs for nvim Lua API
 
 	--  ╭──────────────────────────────────────────────────────────╮
 	--  │                         Snippets                         │
@@ -134,16 +135,16 @@ return require('packer').startup(function(use)
 	--  ╰──────────────────────────────────────────────────────────╯
 
 	setup('cmp', {
-		'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim's built-in LSP
-		'hrsh7th/cmp-path', -- nvim-cmp source for filesystem paths.
-		'hrsh7th/cmp-cmdline', -- nvim-cmp source for vim's cmdline.
-		'hrsh7th/cmp-buffer', -- nvim-cmp source for buffer words
-		'hrsh7th/cmp-nvim-lua', -- nvim-cmp source for Neovim Lua API.
+		'hrsh7th/cmp-nvim-lsp',                -- nvim-cmp source for neovim's built-in LSP
+		'hrsh7th/cmp-path',                    -- nvim-cmp source for filesystem paths.
+		'hrsh7th/cmp-cmdline',                 -- nvim-cmp source for vim's cmdline.
+		'hrsh7th/cmp-buffer',                  -- nvim-cmp source for buffer words
+		'hrsh7th/cmp-nvim-lua',                -- nvim-cmp source for Neovim Lua API.
 		'davidsierradz/cmp-conventionalcommits', -- autocomplete conventional commits
-		'saadparwaiz1/cmp_luasnip', -- completion engine
-		'lukas-reineke/cmp-under-comparator', -- better sort completion items that start with one or more underlines
-		'onsails/lspkind.nvim', -- Auto completions gui tipo do vscode
-		'zbirenbaum/copilot.lua', -- Copilot
+		'saadparwaiz1/cmp_luasnip',            -- completion engine
+		'lukas-reineke/cmp-under-comparator',  -- better sort completion items that start with one or more underlines
+		'onsails/lspkind.nvim',                -- Auto completions gui tipo do vscode
+		'zbirenbaum/copilot.lua',              -- Copilot
 		'zbirenbaum/copilot-cmp',
 	})
 
@@ -152,13 +153,13 @@ return require('packer').startup(function(use)
 	--  ╰──────────────────────────────────────────────────────────╯
 
 	setup('treesitter', {
-		'nvim-treesitter/nvim-treesitter-refactor', -- Refactorings via treesitter
+		'nvim-treesitter/nvim-treesitter-refactor',                          -- Refactorings via treesitter
 		{ 'nvim-treesitter/nvim-treesitter-textobjects', commit = '8673926' }, -- Additional text objects via treesitter
 		'RRethy/nvim-treesitter-textsubjects',
-		'windwp/nvim-ts-autotag', -- tag completion
-		'JoosepAlviste/nvim-ts-context-commentstring', -- conserta comments para jsx,
-		'RRethy/nvim-treesitter-endwise', -- Coloca o end no final de func e if
-		'fladson/vim-kitty', -- syntax highlighting for kitty
+		'windwp/nvim-ts-autotag',                                            -- tag completion
+		'JoosepAlviste/nvim-ts-context-commentstring',                       -- conserta comments para jsx,
+		'RRethy/nvim-treesitter-endwise',                                    -- Coloca o end no final de func e if
+		'fladson/vim-kitty',                                                 -- syntax highlighting for kitty
 	})
 
 	--  ╭──────────────────────────────────────────────────────────╮
@@ -167,18 +168,18 @@ return require('packer').startup(function(use)
 
 	setup('dev.flutter', {
 		'Nash0x7E2/awesome-flutter-snippets', -- snippets
-		'akinsho/pubspec-assist.nvim', -- add/update dart dependencies
+		'akinsho/pubspec-assist.nvim',      -- add/update dart dependencies
 	})
 
 	use('jose-elias-alvarez/typescript.nvim') -- + funcionalidades pro ts_server (e.g. rename file & update imports)
-	use('Vimjas/vim-python-pep8-indent') -- Conserta o indent do python
+	use('Vimjas/vim-python-pep8-indent')     -- Conserta o indent do python
 
 	--  ╭──────────────────────────────────────────────────────────╮
 	--  │                           Git                            │
 	--  ╰──────────────────────────────────────────────────────────╯
 
 	setup('git.vim-fugitive') -- base git plugin
-	setup('git.gitsigns') -- gutter signs
+	setup('git.gitsigns')    -- gutter signs
 
 	--  ╭──────────────────────────────────────────────────────────╮
 	--  │                          Debug                           │
