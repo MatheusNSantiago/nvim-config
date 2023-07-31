d413bc4678d8dbcace1a47bc05b589018cb660b2
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-30 22:20:49 -0300

    chore: supress linting errors

--- lua/plugins/cmp/utils/comparators.lua
+++ lua/plugins/cmp/utils/comparators.lua
@@ -1,7 +1,5 @@
 local M = {}
 
----@param entry1 cmp.Entry
---@param entry2 cmp.Entry
 function M.prioritizeVariables(entry1, entry2)
 	local types = require('cmp.types')
 	local kind1 = types.lsp.CompletionItemKind[entry1:get_kind()]
@@ -9,7 +7,6 @@ function M.prioritizeVariables(entry1, entry2)
 
 	if kind1 ~= kind2 then
 		local is_in_the_file = entry2:get_offset() ~= 0
-		local validFiletypes = not (vim.tbl_contains({ 'css' }, vim.bo.filetype))
 		if kind1 ~= 'Variable' and kind2 == 'Variable' and is_in_the_file then return false end
 	end
 end
--- lua/plugins/debug/ui.lua
+++ lua/plugins/debug/ui.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 local M = {}
 
 function M.setup()
