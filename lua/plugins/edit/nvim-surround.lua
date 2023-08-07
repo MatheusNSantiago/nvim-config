0a25aaf1e5a06faba65335b8cd5ac80e6807a343
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-08-07 14:36:23 -0300

    fix(nvim-surround): make change surrounds work again

--- lua/plugins/edit/nvim-surround.lua
+++ lua/plugins/edit/nvim-surround.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
   return {
     'kylechui/nvim-surround',
-    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    version = '*', -- Use for stability; omit to use `main` branch "for the latest features"
     event = 'VeryLazy',
     config = M.config,
   }
@@ -24,179 +24,6 @@ function M.config()
       change = 'cs',
       change_line = 'cS',
     },
-    surrounds = {
-      ['('] = {
-        add = { '( ', ' )' },
-        find = function() return M.get_selection({ motion = 'a(' }) end,
-        delete = '^(. ?)().-( ?.)()$',
-      },
-      [')'] = {
-        add = { '(', ')' },
-        find = function() return M.get_selection({ motion = 'a)' }) end,
-        delete = '^(.)().-(.)()$',
-      },
-      ['{'] = {
-        add = { '{ ', ' }' },
-        find = function() return M.get_selection({ motion = 'a{' }) end,
-        delete = '^(. ?)().-( ?.)()$',
-      },
-      ['}'] = {
-        add = { '{', '}' },
-        find = function() return M.get_selection({ motion = 'a}' }) end,
-        delete = '^(.)().-(.)()$',
-      },
-      ['<'] = {
-        add = { '< ', ' >' },
-        find = function() return M.get_selection({ motion = 'a<' }) end,
-        delete = '^(. ?)().-( ?.)()$',
-      },
-      ['>'] = {
-        add = { '<', '>' },
-        find = function() return M.get_selection({ motion = 'a>' }) end,
-        delete = '^(.)().-(.)()$',
-      },
-      ['['] = {
-        add = { '[ ', ' ]' },
-        find = function() return M.get_selection({ motion = 'a[' }) end,
-        delete = '^(. ?)().-( ?.)()$',
-      },
-      [']'] = {
-        add = { '[', ']' },
-        find = function() return M.get_selection({ motion = 'a]' }) end,
-        delete = '^(.)().-(.)()$',
-      },
-      ["'"] = {
-        add = { "'", "'" },
-        find = function() return M.get_selection({ motion = "a'" }) end,
-        delete = '^(.)().-(.)()$',
-      },
-      ['"'] = {
-        add = { '"', '"' },
-        find = function() return M.get_selection({ motion = 'a"' }) end,
-        delete = '^(.)().-(.)()$',
-      },
-      ['`'] = {
-        add = { '`', '`' },
-        find = function() return M.get_selection({ motion = 'a`' }) end,
-        delete = '^(.)().-(.)()$',
-      },
-      ['i'] = { -- TODO: Add find/delete/change functions
-        add = function()
-          local left_delimiter = M.get_input('Enter the left delimiter: ')
-          local right_delimiter = left_delimiter and M.get_input('Enter the right delimiter: ')
-          if right_delimiter then return { { left_delimiter }, { right_delimiter } } end
-        end,
-        find = function() end,
-        delete = function() end,
-      },
-      ['t'] = {
-        add = function()
-          local user_input = M.get_input('Enter the HTML tag: ')
-          if user_input then
-            local element = user_input:match('^<?([^%s>]*)')
-            local attributes = user_input:match('^<?[^%s>]*%s+(.-)>?$')
-
-            local open = attributes and element .. ' ' .. attributes or element
-            local close = element
-
-            return { { '<' .. open .. '>' }, { '</' .. close .. '>' } }
-          end
-        end,
-        find = function() return M.get_selection({ motion = 'at' }) end,
-        delete = '^(%b<>)().-(%b<>)()$',
-        change = {
-          target = '^<([^%s<>]*)().-([^/]*)()>$',
-          replacement = function()
-            local user_input = M.get_input('Enter the HTML tag: ')
-            if user_input then
-              local element = user_input:match('^<?([^%s>]*)')
-              local attributes = user_input:match('^<?[^%s>]*%s+(.-)>?$')
-
-              local open = attributes and element .. ' ' .. attributes or element
-              local close = element
-
-              return { { open }, { close } }
-            end
-          end,
-        },
-      },
-      ['T'] = {
-        add = function()
-          local user_input = M.get_input('Enter the HTML tag: ')
-          if user_input then
-            local element = user_input:match('^<?([^%s>]*)')
-            local attributes = user_input:match('^<?[^%s>]*%s+(.-)>?$')
-
-            local open = attributes and element .. ' ' .. attributes or element
-            local close = element
-
-            return { { '<' .. open .. '>' }, { '</' .. close .. '>' } }
-          end
-        end,
-        find = function() return M.get_selection({ motion = 'at' }) end,
-        delete = '^(%b<>)().-(%b<>)()$',
-        change = {
-          target = '^<([^>]*)().-([^/]*)()>$',
-          replacement = function()
-            local user_input = M.get_input('Enter the HTML tag: ')
-            if user_input then
-              local element = user_input:match('^<?([^%s>]*)')
-              local attributes = user_input:match('^<?[^%s>]*%s+(.-)>?$')
-
-              local open = attributes and element .. ' ' .. attributes or element
-              local close = element
-
-              return { { open }, { close } }
-            end
-          end,
-        },
-      },
-      ['f'] = {
-        add = function()
-          local result = M.get_input('Enter the function name: ')
-          if result then return { { result .. '(' }, { ')' } } end
-        end,
-        find = function()
-          if vim.g.loaded_nvim_treesitter then
-            local selection = M.get_selection({
-              query = {
-                capture = '@call.outer',
-                type = 'textobjects',
-              },
-            })
-            if selection then return selection end
-          end
-          return M.get_selection({ pattern = '[^=%s%(%){}]+%b()' })
-        end,
-        delete = '^(.-%()().-(%))()$',
-        change = {
-          target = '^.-([%w_]+)()%(.-%)()()$',
-          replacement = function()
-            local result = M.get_input('Enter the function name: ')
-            if result then return { { result }, { '' } } end
-          end,
-        },
-      },
-      invalid_key_behavior = {
-        add = function(char)
-          if not char or char:find('%c') then return nil end
-          return { { char }, { char } }
-        end,
-        find = function(char)
-          if not char or char:find('%c') then return nil end
-          return M.get_selection({
-            pattern = vim.pesc(char) .. '.-' .. vim.pesc(char),
-          })
-        end,
-        delete = function(char)
-          if not char or char:find('%c') then return nil end
-          return M.get_selections({
-            char = char,
-            pattern = '^(.)().-(.)()$',
-          })
-        end,
-      },
-    },
     aliases = {
       ['a'] = '>',
       ['b'] = ')',
@@ -205,9 +32,7 @@ function M.config()
       ['q'] = { '"', "'", '`' },
       ['s'] = { '}', ']', ')', '>', '"', "'", '`' },
     },
-    highlight = {
-      duration = 0,
-    },
+    highlight = { duration = 0 },
     move_cursor = 'begin',
     indent_lines = function(start, stop)
       local b = vim.bo
