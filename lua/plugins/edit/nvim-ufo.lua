fdeee9b545e472d8cf94c746a7798129367ed497
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-09-07 20:24:21 -0300

    fix(nvim-ufo): fix the folding of all lines on bufread

--- lua/plugins/edit/nvim-ufo.lua
+++ lua/plugins/edit/nvim-ufo.lua
@@ -74,23 +74,23 @@ function M.config()
 
   ufo.setup({
     fold_virt_text_handler = handler,
-    provider_selector = function(_, filetype, buftype)
-      local function handleFallbackException(bufnr, err, providerName)
-        if type(err) == 'string' and err:match('UfoFallbackException') then
-          return ufo.getFolds(bufnr, providerName)
-        else
-          return require('promise').reject(err)
-        end
-      end
-
-      return (filetype == '' or buftype == 'nofile') and 'indent' -- only use indent until a file is opened
-          or function(bufnr)
-            return ufo
-                .getFolds(bufnr, 'lsp')
-                :catch(function(err) return handleFallbackException(bufnr, err, 'treesitter') end)
-                :catch(function(err) return handleFallbackException(bufnr, err, 'indent') end)
-          end
-    end,
+    -- provider_selector = function(_, filetype, buftype)
+    --   local function handleFallbackException(bufnr, err, providerName)
+    --     if type(err) == 'string' and err:match('UfoFallbackException') then
+    --       return ufo.getFolds(bufnr, providerName)
+    --     else
+    --       return require('promise').reject(err)
+    --     end
+    --   end
+    --
+    --   return (filetype == '' or buftype == 'nofile') and 'indent' -- only use indent until a file is opened
+    --       or function(bufnr)
+    --         return ufo
+    --             .getFolds(bufnr, 'lsp')
+    --             :catch(function(err) return handleFallbackException(bufnr, err, 'treesitter') end)
+    --             :catch(function(err) return handleFallbackException(bufnr, err, 'indent') end)
+    --       end
+    -- end,
   })
 end
 
