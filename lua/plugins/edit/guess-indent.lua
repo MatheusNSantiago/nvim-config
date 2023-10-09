0d96d2e50321eb26cfefb9e10ee2b4beb16530c2
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-10-08 22:31:33 -0300

    feat(guess-indent): exclude cobol filetypes

--- lua/plugins/edit/guess-indent.lua
+++ lua/plugins/edit/guess-indent.lua
@@ -12,6 +12,7 @@ function M.config()
     auto_cmd = true,             -- Set to false to disable automatic execution
     override_editorconfig = false, -- Set to true to override settings set by .editorconfig
     filetype_exclude = {         -- A list of filetypes for which the auto command gets disabled
+      'cobol',
       'netrw',
       'tutor',
     },
