378c32f70a979ae33074ad60b71da89b9c5e8861
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-10-13 23:39:00 -0300

    feat(cobol): add default cobol runtimes

--- lua/plugins/dev/cobol.lua
+++ lua/plugins/dev/cobol.lua
@@ -12,6 +12,9 @@ function M.config()
   vim.g.cobol_folding = 1
   vim.g.cobol_autoupper = 1
   vim.g.cobol_indent_data_items = 1
+  vim.g.cobol_indent_id_paras = 1
+  vim.g.cobol_syntax_compl = 0
+  vim.g.cobol_format_free = 0
 end
 
 return M
