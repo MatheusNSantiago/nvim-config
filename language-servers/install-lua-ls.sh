22adc8740a7234e406cff93d29445c18a26a29e7
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-10-13 21:17:14 -0300

    chore(lsp): remove lua_ls install script

--- language-servers/install-lua-ls.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-INSTALL_FOLDER="$HOME/.local/share/nvim/language-servers"
-
-makedir -p "$INSTALL_FOLDER"
-rm -rf "$INSTALL_FOLDER/lua-language-server"
-git clone https://github.com/LuaLS/lua-language-server "$INSTALL_FOLDER/lua-language-server"
-
-cd "$INSTALL_FOLDER/lua-language-server" && ./make.sh
