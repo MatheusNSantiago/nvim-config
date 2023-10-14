7c08968a2025501793c7c28ddcf054a02ceb8de8
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-10-13 23:40:41 -0300

    feat(symbol-usage): add custom format refs, exclude cobol

--- lua/plugins/lsp/symbol-usage.lua
+++ lua/plugins/lsp/symbol-usage.lua
@@ -10,34 +10,51 @@ end
 
 function M.config()
   local SymbolKind = vim.lsp.protocol.SymbolKind
-
   local default_opts = {
     ---@type table<string, any> `nvim_set_hl`-like options for highlight virtual text
     hl = { link = 'Comment' },
     kinds = { SymbolKind.Function, SymbolKind.Method },
+    ---Additional filter for kinds. Recommended use in the filetypes override table.
+    ---fiterKind: function(data: { symbol:table, parent:table, bufnr:integer }): boolean
+    ---`symbol` and `parent` is an item from `textDocument/documentSymbol` request
+    ---See: #filter-kinds
+    kinds_filter = {},
     ---@type 'above'|'end_of_line'|'textwidth'
     vt_position = 'end_of_line',
+    ---Text to display when request is pending. If `false`, extmark will not be
+    ---created until the request is finished. Recommended to use with `above`
+    ---vt_position to avoid "jumping lines".
+    ---@type string|table|false
+    request_pending_text = false,
     ---@type function(symbol: Symbol): string
     text_format = function(symbol)
-      local fragments = {}
+      vim.api.nvim_set_hl(0, 'SymbolUsageRounding', { fg = c.bg_highlight })
+      vim.api.nvim_set_hl(0, 'SymbolUsageContent', { bg = c.bg_highlight, fg = c.gray })
+      vim.api.nvim_set_hl(0, 'SymbolUsageRef', { fg = c.gray, bg = c.bg_highlight })
+
+      local res = {}
       local number_of_references = symbol.references
 
+      local round_start = { '', 'SymbolUsageRounding' }
+      local round_end = { '', 'SymbolUsageRounding' }
+
       if number_of_references then
-        if number_of_references > 0 then
-          table.insert(fragments, (' %s'):format(number_of_references)) --
-        end
+        table.insert(res, round_start)
+        table.insert(res, { ' ', 'SymbolUsageRef' }) --
+        table.insert(res, { tostring(number_of_references), 'SymbolUsageContent' })
+        table.insert(res, round_end)
       end
 
-      if symbol.definition then table.insert(fragments, symbol.definition .. ' defs') end
-
-      if symbol.implementation then table.insert(fragments, symbol.implementation .. ' impls') end
-
-      return table.concat(fragments, ', ')
+      return res
     end,
     references = { enabled = true, include_declaration = false },
     definition = { enabled = false },
     implementation = { enabled = false },
-    filetypes = {},
+    filetypes = {
+      cobol = {
+        kinds = {}, -- não exibir nada para cobol
+      },
+    },
   }
 
   require('symbol-usage').setup(default_opts)
