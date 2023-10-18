3b37ee2246a4422683ab782f35f88f280965dbe4
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-10-17 22:13:09 -0300

    feat(cobol): add stop word mation at '@,_,-"

--- lua/filetypes/cobol/init.lua
+++ lua/filetypes/cobol/init.lua
@@ -5,5 +5,7 @@ return {
   opt = {
     colorcolumn = '7,11,73,80',
     commentstring = '      *%s',
+    -- cria as stop word motion '_', '@' e '-' (não sei como setou o -, mas é a vida ;-;)
+    iskeyword = '@,48-57,_,192-255',
   },
 }
