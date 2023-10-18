18599976fc8e1eae45a45641ab8bb39ae05e6068
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-10-18 19:43:29 -0300

    Revert "chore(js/ts): remove ts go to def keymap"
    This reverts commit 6a661ddb6c4dfe97add674a7079b31cdcbc380c7.

--- lua/lsp/servers/tsserver.lua
+++ lua/lsp/servers/tsserver.lua
@@ -76,7 +76,10 @@ local handlers = {
 }
 
 local on_attach = function(client, bufnr)
-    require('lsp').common_on_attach(client, bufnr) --
+    require('lsp').common_on_attach(client, bufnr)
+
+    utils.api.keymap('n', 'gd', ':TypescriptGoToSourceDefinition<CR>')
+    -- keymap('n', 'K', ':Lspsaga hover_doc<CR>')
 end
 
 return {
