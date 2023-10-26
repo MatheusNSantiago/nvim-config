577ca78269ce6085bbe805eff4622cf2ac37b9d3
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-10-26 07:57:22 -0300

    feat(filetypes/js): add helper js mappings for ts dev

--- lua/filetypes/js.lua
+++ lua/filetypes/js.lua
@@ -9,6 +9,7 @@ return {
     )
   end,
   mappings = {
+    { 'n', '<leader>r', ":TermExec cmd='tree-sitter generate && tree-sitter parse examples/foo.cbl'<CR>"},
     { 'n', 'gd', ':TypescriptGoToSourceDefinition<CR>',             desc = 'typescript: go to source definition' },
     { 'n', 'gD', ':tab split | TypescriptGoToSourceDefinition<CR>', desc = 'typescript: go to source definition' },
     {
