9b7b2a2232da091ab1d6991ba5a018a81108d0bf
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-11-06 23:24:18 -0300

    feat(cobol/plugins): make comments work on empty lines

--- lua/filetypes/cobol/plugins.lua
+++ lua/filetypes/cobol/plugins.lua
@@ -9,20 +9,23 @@ return {
     plugin.setup({
       sticky = true,
       pre_hook = function() return '%s' end, -- previnir o default "     *%s" (que não funfava)
-      post_hook = function(ctx)           -- cria um toggle comment functional
+      --- cria um toggle comment functional
+      post_hook = function(ctx)
         local row_start = ctx.range.srow
         local row_end = ctx.range.erow
 
         local lines = vim.api.nvim_buf_get_lines(0, row_start - 1, row_end, false)
         for i, line in ipairs(lines) do
-          local is_populated = #line > 7
-
+          local is_populated = #line > 6
           if is_populated then
             local is_commented = string.sub(line, 7, 7) == '*'
 
             local replacement_char = is_commented and ' ' or '*'
             -- coloca o replacement_char na posição 7
             lines[i] = string.sub(line, 0, 6) .. replacement_char .. string.sub(line, 8, #line)
+          else
+            local cols_to_fill = 6 - #line
+            lines[i] = line .. string.rep(' ', cols_to_fill) .. '*'
           end
         end
 
