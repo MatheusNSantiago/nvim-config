2d6356c82b75d165f948012872c603b13d272698
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-11-30 13:09:53 -0300

    chore(cobol/plugins): bring comment function back to this file

--- lua/filetypes/cobol/plugins.lua
+++ lua/filetypes/cobol/plugins.lua
@@ -1,9 +1,7 @@
-local U = require('filetypes.cobol.utils')
 return {
   ['comment-box'] = function(plugin) plugin.setup({ box_width = 64, line_width = 64 }) end,
   ['Comment'] = function(plugin)
     plugin.setup({
-
       sticky = true,
       pre_hook = function() return '%s' end, -- previnir o default "     *%s" (que não funfava)
       --- cria um toggle comment functional
@@ -13,7 +11,22 @@ return {
         local row_start = ctx.range.srow
         local row_end = ctx.range.erow
 
-        U.comment_lines(row_start, row_end)
+        local lines = vim.api.nvim_buf_get_lines(0, row_start - 1, row_end, false)
+        for i, line in ipairs(lines) do
+          local is_populated = #line > 6
+          if is_populated then
+            local is_commented = string.sub(line, 7, 7) == '*'
+
+            local replacement_char = is_commented and ' ' or '*'
+            -- coloca o replacement_char na posição 7
+            lines[i] = string.sub(line, 0, 6) .. replacement_char .. string.sub(line, 8, #line)
+          else
+            local cols_to_fill = 6 - #line
+            lines[i] = line .. string.rep(' ', cols_to_fill) .. '*'
+          end
+        end
+
+        vim.api.nvim_buf_set_lines(0, row_start - 1, row_end, false, lines)
       end,
     })
   end,
