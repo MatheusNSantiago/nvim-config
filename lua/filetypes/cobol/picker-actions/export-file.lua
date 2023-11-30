c7d6ff73ac2d821f9a12108d6f7250cc2d3d423f
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-11-30 13:11:36 -0300

    feat(cobol/picker): implement export cobol to the BB standard

--- lua/filetypes/cobol/picker-actions/export-file.lua
+++ lua/filetypes/cobol/picker-actions/export-file.lua
@@ -1,65 +1,62 @@
 local U = require('filetypes.cobol.utils')
-local ts, fn = vim.treesitter, vim.fn
 local M = {}
 
-function M.main()
-  local marked_lines = Array() -- Linhas que serão comentadas
+function M.export_file()
+  local lines = vim.fn.getline(1, '$')
+  local marked_lines = Set() -- Linhas que serão descomentadas
 
-  local lines = fn.getline(1, '$')
   for i, line in ipairs(lines) do
     -- Se a linha estiver marcada, não faz nada
     if marked_lines:contains(i) then goto continue end
 
-    if M.line_has_copy_statement(line) then --
-      line = M.replace_copy_with_inc(line)
-    end
+    line = M.replace_copy_with_inc(line)
 
-    if M.is_call_statement(line) then
-      local row, col = i, M.call_statement_col(line)
-      local start, end_ = M.call_statement_range(row, col)
-
-      local lines_to_comment = fn.range(start, end_)
-      marked_lines:extend(lines_to_comment)
+    if M.is_commented_call_statement(line) then
+      local statement_lines = M.commented_call_statement_lines({ idx = i, line = line })
+      marked_lines:extend(statement_lines)
     end
 
     lines[i] = line
     ::continue::
   end
 
-  marked_lines:foreach(function(lnum) --
-    lines[lnum] = M.comment_line(lines[lnum])
+  -- Descomenta as linhas marcadas
+  marked_lines:foreach(function(lnum)
+    local line = lines[lnum]
+    lines[lnum] = U.uncomment_line(line)
   end)
 
-  fn.setreg('+', lines)
+  vim.fn.setreg('+', lines)
   print('Arquivo exportado para o clipboard')
 end
 
-M.is_call_statement = function(line) return line:find('^%s+CALL') ~= nil end
-M.call_statement_col = function(line)
-  local _, end_col = line:find('^%s+CALL')
-  return end_col
-end
+--  ╾───────────────────────────────────────────────────────────────────────────────────╼
 
-M.line_has_copy_statement = function(line) return line:find(U.spaces(7) .. 'COPY%s+[a-zA-Z0-9]+%s*%.') ~= nil end
-M.replace_copy_with_inc = function(line) return line:gsub(U.spaces(7) .. 'COPY%s+([a-zA-Z0-9]+)%s*%.', '-INC %1') end
+function M.commented_call_statement_lines(opts)
+  local line_idx, line = opts.idx, opts.line
+  local lines = Set()
 
-M.call_statement_range = function(row, col)
-  local pos = { row - 1, col - 1 }
 
-  local node = ts.get_node({ pos = pos })
-  assert(node ~= nil, ('node da call statement na posicao (%d, %d) não encontrado'):format(row, col))
+  while U.is_comment(line) do
+    lines:push(line_idx)
 
-  local s, _, e, _ = node:range()
-  return s + 1, e + 1
-end
+    local is_end_of_statement = line:find('^%s+%*%s*[a-zA-Z0-9-]+%.')
+    if is_end_of_statement then break end
 
-function M.comment_line(line)
-  if U.is_comment(line) then return line end
-  local is_populated = #line > 6
-  if is_populated then --
-    return string.sub(line, 0, 6) .. '*' .. string.sub(line, 8, #line)
+    line_idx = line_idx + 1
+    line = vim.fn.getline(line_idx)
   end
-  return line .. U.spaces(6 - #line) .. '*'
+
+  return lines
 end
 
-return M.main
+M.is_commented_call_statement = function(line)
+  local pattern = U.spaces(6) .. '%*' .. '%s*CALL'
+  return line:find(pattern) ~= nil
+end
+M.replace_copy_with_inc = function(line)
+  local pattern = U.spaces(7) .. 'COPY%s+([a-zA-Z0-9]+)%s*%.'
+  return line:gsub(pattern, '-INC %1')
+end
+
+return M.export_file
