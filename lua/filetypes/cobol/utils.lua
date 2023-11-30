1629c3b2d10f7600fe7c2fd17bcfb2dc34eccf9f
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-11-30 13:07:30 -0300

    feat(cobol/utils): add helpers for commenting and uncommenting lines

--- lua/filetypes/cobol/utils.lua
+++ lua/filetypes/cobol/utils.lua
@@ -2,23 +2,6 @@ local M = {}
 
 function M.is_comment(line) return (#line > 6) and (string.sub(line, 7, 7) == '*') end
 
-function M.comment_lines(row_start, row_end, buf)
-  local lines = vim.api.nvim_buf_get_lines(0, row_start - 1, row_end, false)
-  for i, line in ipairs(lines) do
-    local is_populated = #line > 6
-    if is_populated then
-      local replacement_char = M.is_comment(line) and ' ' or '*'
-
-      -- coloca o replacement_char na posição 7
-      lines[i] = string.sub(line, 0, 6) .. replacement_char .. string.sub(line, 8, #line)
-    else
-      lines[i] = line .. M.spaces(6 - #line) .. '*'
-    end
-  end
-
-  vim.api.nvim_buf_set_lines(buf or 0, row_start - 1, row_end, false, lines)
-end
-
 ---@param word_pattern? string: default: 'A-Za-z0-9-_'
 ---@return string | nil
 function M.get_word_under_cursor(word_pattern)
@@ -35,4 +18,19 @@ end
 
 M.rep = function(s, n) return string.rep(s, n) end
 M.spaces = function(n) return string.rep(' ', n) end
+
+function M.comment_line(line)
+  if M.is_comment(line) then return line end
+  local is_populated = #line > 6
+  if is_populated then --
+    return string.sub(line, 0, 6) .. '*' .. string.sub(line, 8, #line)
+  end
+  return line .. M.spaces(6 - #line) .. '*'
+end
+
+function M.uncomment_line(line)
+  if not M.is_comment(line) then return line end
+  return string.sub(line, 0, 6) .. ' '.. string.sub(line, 8, #line)
+end
+
 return M
