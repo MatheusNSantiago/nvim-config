7cbffa2dc9398f0d78650de5efa98af255163884
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-12-04 17:41:13 -0300

    chore(cobol/indent): remove useless lines

--- lua/filetypes/cobol/indent.lua
+++ lua/filetypes/cobol/indent.lua
@@ -52,7 +52,7 @@ local function is_cursor_on_division(division)
 end
 
 local function indent(size)
-  vim.schedule(function()
+  vim.defer_fn(function()
     local line = fn.getline('.')
     local line_number = fn.line('.')
     local col = fn.col('.')
@@ -61,7 +61,7 @@ local function indent(size)
 
     fn.setline(line_number, indented_line)
     fn.cursor({ line_number, col + size })
-  end)
+  end, 20)
 end
 
 local function is_first_relevant_line_after(lnum, pattern)
@@ -81,29 +81,30 @@ local function is_first_relevant_line_after(lnum, pattern)
 end
 
 local function insert_line_below()
-  local mode = vim.fn.mode()
-  if mode == 'i' then
-    vim.cmd('stopinsert')
-    vim.api.nvim_feedkeys('o', 'n', true)
-    vim.cmd('startinsert')
-  else
-    vim.api.nvim_feedkeys('o', 'n', true)
+  if fn.mode() == 'i' then
+    utils.api.feedkeys('<CR>')
+    return
   end
+  utils.api.feedkeys('o')
 end
 
 function M.new_indentedline_below()
-  local is_cursor_in_proc_div = is_cursor_on_division('procedure')
+  local ok, is_cursor_in_proc_div = pcall(is_cursor_on_division, 'procedure')
+  if not ok then return insert_line_below() end
+
   local lnum, line = fn.line('.'), fn.getline('.')
   local line_is_comment = U.is_comment(line)
 
   insert_line_below()
 
   if is_cursor_in_proc_div then
+    -- relevante = não é comentário, não é divisão, não é linha em branco
     local is_first_relevant_line_after_section = is_first_relevant_line_after(lnum, 'SECTION%.%s*$')
-
     if is_first_relevant_line_after_section then
       -- Por algum motivo, quando tem um comentário, o tamanho do indent diminui
-      if line_is_comment then return indent(5) end
+      if line_is_comment then
+        return indent(5)
+      end
 
       return indent(4)
     end
@@ -112,97 +113,13 @@ function M.new_indentedline_below()
     local is_before_sec_a = fn.col('.') < 7
     if is_first_relevant_line_after_division and is_before_sec_a then return end
   end
-
-  local is_root_of_group = line:match('^%s+01 .+%.$')
-  if is_root_of_group then return indent(4) end
-
-  -- o indent padrão identa na msm coluna do inicio do comentário, queremos que ele começe
-  -- na primeira coluna válida
-  if line_is_comment then return indent(1) end
+  --
+  -- local is_root_of_group = line:match('^%s+01 .+%.$')
+  -- if is_root_of_group then return indent(4) end
+  --
+  -- -- o indent padrão identa na msm coluna do inicio do comentário, queremos que ele começe
+  -- -- na primeira coluna válida
+  -- if line_is_comment then return indent(1) end
 end
 
 return M
-
---  ╾───────────────────────────────────────────────────────────────────────────────────╼
--- local lsp = require('filetypes.cobol.lsp')
---
--- local M = {
---   current_line = '',
---   current_line_raw = '',
--- }
--- M.indent = function()
---   M.current_line_raw = vim.fn.getline('.')
---   M.current_line = M.get_current_line()
---
---   M.add_new_line()
---
---   lsp.tree_provider(function(tree)
---     local is_cursor_in_procedure_division = M.is_cursor_in_node('PROCEDURE DIVISION', tree)
---     local has_7_spaces_before_char = M.blanklines_util_char() == 7
---
---
---     if is_cursor_in_procedure_division and has_7_spaces_before_char then
---       return M.add_indentation(4)
---     end
---
---     if M.is_division() then return M.add_indentation(1) end
---     if M.is_section() then M.add_indentation(2) end
---     if M.is_root_of_group() then M.add_indentation(3) end
---   end)
--- end
---
--- M._end_of_line = '%s*█%s*$'
--- M.is_division = function() return M._line_matches({ 'DIVISION%.' .. M._end_of_line }) end
--- M.is_section = function() return M._line_matches({ 'SECTION%.' .. M._end_of_line }) end
--- M.is_root_of_group = function() return M._line_matches({ '01 .+%.' .. M._end_of_line }) end
---
--- M.is_cursor_in_node = function(node_name, tree)
---   local node = lsp.search_node(node_name, tree)
---   if node == nil then return false end
---   local cursor_pos = vim.fn.line('.')
---
---   local node_start, node_end = node.range.start, node.range.finish
---
---   if (cursor_pos <= node_end) and (cursor_pos >= node_start) then return true end
---   return false
--- end
---
--- function M.blanklines_util_char()
---   local line = M.current_line_raw
---   local line_with_no_leading_spaces = line:gsub('^%s+', '')
---
---   return #line - #line_with_no_leading_spaces
--- end
---
--- function M.get_current_line()
---   local current_line = vim.fn.getline('.')
---   local col = vim.fn.col('.')
---   current_line = (current_line:sub(1, col) .. '█' .. current_line:sub(col + 1)):upper()
---
---   return current_line
--- end
---
--- function M._line_matches(patterns)
---   for _, pattern in ipairs(patterns) do
---     if M.current_line:match(pattern) then return true end
---   end
---   return false
--- end
---
--- M.add_new_line = function()
---   local new_line = vim.api.nvim_replace_termcodes('<C-CR>', true, false, true)
---   vim.api.nvim_feedkeys(new_line, 'i', true)
--- end
---
--- M.add_indentation = function(size)
---   vim.schedule(function()
---     local line = vim.fn.getline('.')
---     local line_number = vim.fn.line('.')
---     local col = vim.fn.col('.')
---
---     vim.fn.setline(line_number, line:sub(0, col - 1) .. string.rep(' ', size) .. line:sub(col, #line))
---     vim.fn.cursor({ line_number, col + size })
---   end)
--- end
-
--- return M.indent
