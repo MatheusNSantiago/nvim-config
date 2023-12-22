e89819b33235d41e1f214ed34373110b961f3179
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-12-21 21:28:07 -0300

    fix(cobol/utils): fix get word under cursor

--- lua/filetypes/cobol/utils.lua
+++ lua/filetypes/cobol/utils.lua
@@ -7,7 +7,8 @@ function M.is_comment(line) return (#line > 6) and (string.sub(line, 7, 7) == '*
 function M.get_word_under_cursor(word_pattern)
   word_pattern = word_pattern or 'A-Za-z0-9-_'
   local cur_line = utils.get_current_line_with_cursor()
-  local pattern = '%W' .. ('([%s]+█[%s]+)'):format(word_pattern, word_pattern) .. '%W'
+  -- local pattern = '%W' .. ('([%s]+█[%s]+)'):format(word_pattern, word_pattern) .. '%W'
+  local pattern = ('([%s]+█[%s]+)'):format(word_pattern, word_pattern)
   local word_under_cursor = cur_line:match(pattern)
 
   if word_under_cursor then
