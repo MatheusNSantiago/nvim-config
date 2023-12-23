26e0ff9c81a9aeab5e6287d82cf04b10603e120d
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-12-22 21:37:23 -0300

    feat(utils/nui/input): add wrapper

--- lua/utils/nui/input.lua
+++ lua/utils/nui/input.lua
@@ -1,57 +1,70 @@
 local NuiInput = require('nui.input')
-local Input = NuiInput:extend('Input')
 
----@class InputOpts
+---@class Input
+local Input = Class('Input')
+
+---@class InputOptions
 ---@field on_submit fun(value: string)
 ---@field on_close? fun(value: string)
 ---@field title? string
 ---@field default_value? string
 ---@field auto_mount? boolean
+---@field size? {width: number, height: number}
 
----@param opts InputOpts
-function Input:init(opts)
-
+---@param opts InputOptions
+function Input:initialize(opts)
   local popup_options = {
+    enter = true,
     relative = 'editor',
     position = '50%',
-    size = { width = 20, height = 1 },
+    size = opts.size or { width = 25, height = 1 },
     border = {
-      style = 'rounded',
-      text = { top = 'Novo book', top_align = 'center' },
+      highlight = 'FloatBorder',
+      style = 'single',
+      text = { top = opts.title, top_align = 'center' },
+      padding = { left = 1 },
     },
   }
 
-  popup_options.win_options = vim.tbl_deep_extend('force', popup_options.win_options or {}, {
-    conceallevel = 2,
-    concealcursor = 'nvi',
+  self.input = NuiInput(popup_options, {
+    on_submit = opts.on_submit,
+    on_close = opts.on_close,
+    -- on_change = function(_) end,
   })
 
-  Input.super.init(self, popup_options, {
-    on_submit = function(value) print(value) end,
-  })
+  self.input:on('BufLeave', function() self.input:unmount() end)
+  self.input:map('n', 'q', function() self.input:unmount() end)
+  self.input:map('i', '<C-c>', function() self.input:unmount() end)
 
-  self._.conceal_char = '*'
+  if opts.auto_mount then self:mount() end
 end
 
-function Input:mount()
-  Input.super.mount(self)
+function Input:mount() self.input:mount() end
+function Input:unmount() self.input:unmount() end
 
-  local conceal_char = self._.conceal_char
-  local prompt_length = vim.api.nvim_strwidth(vim.fn.prompt_getprompt(self.bufnr))
+-- popup_options.win_options = vim.tbl_deep_extend('force', popup_options.win_options or {}, {
+--   conceallevel = 2,
+--   concealcursor = 'nvi',
+-- })
+-- local conceal_char = '*'
+-- local prompt_length = 3
 
-  vim.api.nvim_buf_call(
-    self.bufnr,
-    function()
-      vim.cmd(string.format(
-        [[
-        syn region SecretValue start=/^/ms=s+%s end=/$/ contains=SecretChar
-        syn match SecretChar /./ contained conceal %s
-      ]],
-        prompt_length,
-        conceal_char and 'cchar=' .. conceal_char or ''
-      ))
-    end
-  )
-end
+-- vim.api.nvim_buf_call(
+--   self.input.bufnr,
+--   function()
+--     vim.cmd(string.format(
+--       [[
+--       syn region SecretValue start=/^/ms=s+%s end=/$/ contains=SecretChar
+--       syn match SecretChar /./ contained conceal %s
+--     ]],
+--       prompt_length,
+--       conceal_char and 'cchar=' .. conceal_char or ''
+--     ))
+--   end
+-- )
 
-return Input
+--  ╾───────────────────────────────────────────────────────────────────────────────────╼
+---@alias Input.constructor fun(opts: InputOptions): Input
+---@type Input|Input.constructor
+local _Input = Input
+return _Input
