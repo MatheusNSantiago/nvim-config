5831cf6e1bd60eb7ebfb950f844fae19fd1a4f80
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-12-23 10:37:32 -0300

    feat(cobol): add outline section search via telescope

--- lua/filetypes/cobol/init.lua
+++ lua/filetypes/cobol/init.lua
@@ -19,10 +19,11 @@ end
 
 M.mappings = {
   { 'n',          '<leader>o',  outline.open },
+  { 'n',          '<leader>ss', outline.search_section },
   -- { { 'n', 'x' }, 'w',          require('filetypes.cobol.motions').to_start_of_next_word },
   -- { { 'n', 'x' }, 'e',          require('filetypes.cobol.motions').to_end_of_word },
   -- { { 'n', 'x' }, 'b',          require('filetypes.cobol.motions').start_of_previous_word },
-  { 'n',          '<leader>ap', require('cobol-quick-add').add.pic_under_cursor },
+  { 'n',          '<leader>ap', require('cobol-quick-add').add_picture },
   { 'n',          '<leader>as', require('cobol-quick-add').add_section },
   { 'n',          '<leader>r',  require('filetypes.cobol.code-runner').run },
   { 'n',          '<leader>cl', function() require('comment-box').line(5) end },
