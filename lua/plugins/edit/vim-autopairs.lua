56c22b9652ed7e5a5f3a486d6ed698ccb20a3a2d
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-01-14 14:58:30 -0300

    feat(auto-pairs): exclude certain files from completing parenthesis on funcions

--- lua/plugins/edit/vim-autopairs.lua
+++ lua/plugins/edit/vim-autopairs.lua
@@ -9,9 +9,20 @@ function M.setup()
 end
 
 function M.config()
-	-- make autopairs and completion work together
-	local cmp_status_ok, cmp = pcall(require, 'cmp')
-	if cmp_status_ok then cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done()) end
+	local exclude_filetypes = { 'cobol', 'copybook' }
+
+	utils.api.augroup('auto_pairs_and_completion_augroup', {
+		desc = 'make autopairs and completion work together',
+		event = 'BufEnter',
+		pattern = '*',
+		command = function()
+			local ft = vim.bo.filetype
+			if vim.tbl_contains(exclude_filetypes, ft) then return end
+
+			local cmp_status_ok, cmp = pcall(require, 'cmp')
+			if cmp_status_ok then cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done()) end
+		end,
+	})
 
 	require('nvim-autopairs').setup({
 		check_ts = true,
