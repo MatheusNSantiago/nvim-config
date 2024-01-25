db3e930e440158e7190f29383701b8a80d455bef
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-01-25 18:39:38 -0300

    fix(nvim-neoclip): lazy=false to fix not saving yanks

--- lua/plugins/edit/nvim-neoclip.lua
+++ lua/plugins/edit/nvim-neoclip.lua
@@ -4,13 +4,16 @@ function M.setup()
 	return {
 		'AckslD/nvim-neoclip.lua',
 		config = M.config,
-    lazy = true,
+		lazy = false,
 	}
 end
 
 function M.config()
 	require('neoclip').setup({
-		history = 50,
+		history = 1000,
+		enable_persistent_history = false,
+		length_limit = 1048576,
+		continuous_sync = false,
 		filter = nil,
 		preview = true,
 		prompt = nil,
@@ -18,6 +21,7 @@ function M.config()
 		default_register_macros = 'q',
 		enable_macro_history = true,
 		content_spec_column = false,
+		disable_keycodes_parsing = false,
 		on_select = {
 			move_to_front = false,
 			close_telescope = true,
@@ -39,27 +43,77 @@ function M.config()
 			telescope = {
 				i = {
 					select = '<cr>',
-					paste = '<c-p>',
-					paste_behind = '<c-k>',
-					replay = '<c-q>', -- replay a macro
-					delete = '<c-d>', -- delete an entry
-					edit = '<c-e>', -- edit an entry
+					paste = {},  -- '<c-p>',
+					paste_behind = {}, --'<c-k>',
+					replay = {}, --'<c-q>', -- replay a macro
+					delete = {}, --'<c-d>', -- delete an entry
+					edit = {}, --'<c-e>', -- edit an entry
 					custom = {},
 				},
 				n = {
 					select = '<cr>',
 					paste = 'p',
-					--- It is possible to map to more than one key.
-						-- paste = { 'p', '<c-p>' },
 					paste_behind = 'P',
 					replay = 'q',
 					delete = 'd',
 					edit = 'e',
 					custom = {},
 				},
-			}
+			},
 		},
 	})
+
+	-- require('neoclip').setup({
+	-- 	history = 50,
+	-- 	filter = nil,
+	-- 	preview = true,
+	-- 	prompt = nil,
+	-- 	default_register = '"',
+	-- 	default_register_macros = 'q',
+	-- 	enable_macro_history = true,
+	-- 	content_spec_column = false,
+	-- 	on_select = {
+	-- 		move_to_front = false,
+	-- 		close_telescope = true,
+	-- 	},
+	-- 	on_paste = {
+	-- 		set_reg = false,
+	-- 		move_to_front = false,
+	-- 		close_telescope = true,
+	-- 	},
+	-- 	on_replay = {
+	-- 		set_reg = false,
+	-- 		move_to_front = false,
+	-- 		close_telescope = true,
+	-- 	},
+	-- 	on_custom_action = {
+	-- 		close_telescope = true,
+	-- 	},
+	-- 	keys = {
+	-- 		telescope = {
+	-- 			i = {
+	-- 				select = '<cr>',
+	-- 				paste = '<c-p>',
+	-- 				paste_behind = '<c-k>',
+	-- 				replay = '<c-q>', -- replay a macro
+	-- 				delete = '<c-d>', -- delete an entry
+	-- 				edit = '<c-e>', -- edit an entry
+	-- 				custom = {},
+	-- 			},
+	-- 			n = {
+	-- 				select = '<cr>',
+	-- 				paste = 'p',
+	-- 				--- It is possible to map to more than one key.
+	-- 					-- paste = { 'p', '<c-p>' },
+	-- 				paste_behind = 'P',
+	-- 				replay = 'q',
+	-- 				delete = 'd',
+	-- 				edit = 'e',
+	-- 				custom = {},
+	-- 			},
+	-- 		}
+	-- 	},
+	-- })
 end
 
 return M
