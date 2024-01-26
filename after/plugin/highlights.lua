045697910bbac21a7815107fa19211622a97ac24
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-01-25 23:29:41 -0300

    refactor(highlight): move initial setup highlight to autocommand

--- after/plugin/highlights.lua
+++
@@ -1,3 +0,0 @@
-local colorscheme = require('colorscheme')
-
-utils.set_hls(colorscheme.get_all_highlights())
--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,33 +1,37 @@
 local augroup, cmd, fn = utils.api.augroup, vim.cmd, vim.fn
 
-augroup('Não comenta nova linha automaticamente', {
+augroup('my_autocommands_augroup', {
+	desc = 'Não comenta nova linha automaticamente',
 	pattern = '*',
 	event = 'BufEnter',
 	command = [[set formatoptions-=cro]],
-})
-
-augroup('Mover para a última posição quando abrir um buffer', {
+}, {
+	desc = 'Seta os highlights quando entra no vim',
+	event = 'VimEnter',
+	command = function()
+		local hls = require('colorscheme').get_all_highlights()
+		utils.set_hls(hls)
+	end,
+}, {
+	desc = 'Mover para a última posição quando abrir um buffer',
 	pattern = '*',
 	event = 'BufReadPost',
 	command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]],
-})
-
-augroup('Limpar trailing spaces antes de salvar', {
+}, {
+	desc = 'Limpar trailing spaces antes de salvar',
 	event = 'BufWritePre',
 	pattern = '*',
 	command = function() cmd([[ %s/\s\+$//e ]]) end,
-})
-
-augroup('Abrir imagens com o aplicativo padrão', {
+}, {
+	desc = 'Abrir imagens com o aplicativo padrão',
 	event = { 'BufEnter' },
 	pattern = { '*.png', '*.jpg', '*.gif' },
 	command = function()
 		cmd('silent! !open ' .. fn.expand('%'))
 		cmd('silent! :bw!')
 	end,
-})
-
-augroup('Salvar antes de sair', {
+}, {
+	desc = 'Salvar antes de sair',
 	event = 'BufLeave',
 	pattern = '*',
 	command = function()
