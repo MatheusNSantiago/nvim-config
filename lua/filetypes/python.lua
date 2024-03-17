3e585a21a7cab44ef4d4fe3a1a7ef4b9f46a6047
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-03-17 10:46:22 -0300

    feat(filetypes/python): add requirements.txt to the initial project

--- lua/filetypes/python.lua
+++ lua/filetypes/python.lua
@@ -17,7 +17,7 @@ return {
           exec('python -m venv --upgrade-deps .venv')
           exec('source .venv/bin/activate.fish')
           exec('curl -o .gitignore https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore')
-          exec('touch main.py')
+          exec('touch main.py requirements.txt')
         end,
       },
       { name = 'install requirements', handler = 'pip install -r requirements.txt' },
