c4282293985f847425d508b57b9f0c70870fc66f
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-03-17 10:33:26 -0300

    feat(python): add .venv gitignore and adding main file to project action

--- lua/filetypes/python.lua
+++ lua/filetypes/python.lua
@@ -1,14 +1,27 @@
+local function exec(cmd)
+  vim.cmd("TermExec cmd='" .. cmd .. "'")
+  vim.defer_fn(function() vim.cmd('stopinsert') end, 100)
+end
+
 ---@type FiletypeSettings
 return {
-    picker = {
-        keymap = '<leader><leader>o',
-        title = 'Python Commands',
-        actions= {
-            { name = 'Criar venv',           handler = 'python -m venv --upgrade-deps .venv' },
-            { name = 'install requirements', handler = 'pip install -r requirements.txt' },
-        },
+  picker = {
+    keymap = '<leader><leader>o',
+    title = 'Python Commands',
+    actions = {
+      {
+        name = 'Iniciar Projeto',
+        handler = function()
+          exec('python -m venv --upgrade-deps .venv')
+          exec('source .venv/bin/activate.fish')
+          exec('curl -o .gitignore https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore')
+          exec('touch main.py')
+        end,
+      },
+      { name = 'install requirements', handler = 'pip install -r requirements.txt' },
     },
-    mappings = {
-        { 'n', '<leader>rp', [[:w<CR>:TermExec cmd='python "%"'<CR>]], desc = 'python: [R]un [P]rogram' },
-    },
+  },
+  mappings = {
+    { 'n', '<leader>r', [[:w<CR>:TermExec cmd='python "%"'<CR>]], desc = 'python: [R]un' },
+  },
 }
