be6c31f01fe2dc8b3b0a0fedd6ccb7f372f33e8b
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-04-12 14:19:29 -0300

    chore(filetypes): add error handling on missing package

--- lua/filetypes/init.lua
+++ lua/filetypes/init.lua
@@ -1,7 +1,10 @@
 ---@diagnostic disable: missing-fields
 
-local ok, cobol_bundle = pcall(require,'cobol-bundle')
-if not ok then cobol_bundle = {} end
+local cobol_bundle_ok, cobol_bundle = pcall(require, 'cobol-bundle')
+if not cobol_bundle_ok then cobol_bundle = {} end
+
+local cobol_foo_ok, cobol_foo = pcall(require, 'cobol-foo')
+if not cobol_foo_ok then cobol_foo = {} end
 
 utils.ft_helpers.filetype_settings({
   ['python'] = require('filetypes.python'),
@@ -9,9 +12,9 @@ utils.ft_helpers.filetype_settings({
   ['c'] = require('filetypes.c'),
   ['cobol'] = cobol_bundle.cobol_config,
   ['copybook'] = cobol_bundle.copybook_config,
-  ['foo'] = require("cobol-foo"),
+  ['foo'] = cobol_foo,
 })
 
 create_picker('<leader><leader>u', 'Utilities', {
-  { name = 'Icon Picker',  handler = ':IconPickerNormal<CR>' },
+  { name = 'Icon Picker', handler = ':IconPickerNormal<CR>' },
 })
