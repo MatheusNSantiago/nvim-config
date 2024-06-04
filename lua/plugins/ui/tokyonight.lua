8287c2de1a703cfdf0662ed2e7a550b9fd239914
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-06-03 21:23:41 -0300

    feat(tokyonight): disable transparency on wsl

--- lua/plugins/ui/tokyonight.lua
+++ lua/plugins/ui/tokyonight.lua
@@ -3,19 +3,18 @@ local M = {}
 function M.setup()
 	return {
 		'folke/tokyonight.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+		lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
 		config = M.config,
-
 	}
 end
 
 function M.config()
-	local tokyonight = require('tokyonight')
+	local is_wsl = utils.is_os_running_on_wsl()
 
-	tokyonight.setup({
-		style = 'night',   -- night | storm | moon
-		-- transparent = true, -- Enable this to disable setting the background color
+	require('tokyonight').setup({
+		style = 'night',        -- night | storm | moon
+		transparent = not is_wsl, -- Enable this to disable setting the background color
 		terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
 		styles = {
 			-- Style to be applied to different syntax groups
@@ -25,8 +24,8 @@ function M.config()
 			functions = 'bold',
 			variables = 'NONE',
 			-- Background styles. Can be "dark", "transparent" or "normal"
-			-- sidebars = 'transparent', -- style for sidebars, see below
-			-- floats = 'transparent', -- style for floating windows
+			sidebars = is_wsl and 'normal' or 'transparent', -- style for sidebars, see below
+			floats = is_wsl and 'normal' or 'transparent', -- style for floating windows
 		},
 		-- on_highlights = function(hl, _)
 		-- 	for group, colors in pairs(require('colorscheme').get_all_highlights()) do
