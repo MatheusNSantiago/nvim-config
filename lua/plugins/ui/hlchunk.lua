639601c37b1621f9c8fee2f7df74948bf3e6781d
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-06-11 19:35:18 -0300

    fix(hlchunk): remove animations

--- lua/plugins/ui/hlchunk.lua
+++ lua/plugins/ui/hlchunk.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
   return {
     'shellRaining/hlchunk.nvim',
-    event = { 'UIEnter' },
+    event = { 'BufReadPre', 'BufNewFile' },
     config = M.config,
   }
 end
@@ -12,8 +12,9 @@ function M.config()
   require('hlchunk').setup({
     chunk = {
       enable = true,
-      use_treesitter = true,
-      notify = true, -- notify if some situation(like disable chunk mod double time)
+      style = { { fg = '#806d9c' }, { fg = '#c21f30' } }, -- style = { { fg = '#806d9c' } },
+      notify = true,
+      priority = 15,
       exclude_filetypes = {
         aerial = true,
         html = true,
@@ -21,16 +22,18 @@ function M.config()
         cobol = true,
         copybook = true,
       },
-      support_filetypes = {
-        '*.py',
-        '*.lua',
-        '*.jsx?',
-        '*.tsx?',
-        '*.dart',
-        '*.yaml',
-        '*.json',
-        '*.c',
-      },
+      ------------------
+      use_treesitter = true,
+      -- support_filetypes = {
+      --   '*.py',
+      --   '*.lua',
+      --   '*.jsx?',
+      --   '*.tsx?',
+      --   '*.dart',
+      --   '*.yaml',
+      --   '*.json',
+      --   '*.c',
+      -- },
       chars = {
         horizontal_line = '─',
         vertical_line = '│',
@@ -38,7 +41,12 @@ function M.config()
         left_bottom = '╰',
         right_arrow = '─', -- '>',
       },
-      style = { { fg = '#806d9c' } },
+      textobject = 'ac',
+      max_file_size = 1024 * 1024, -- 1MB
+      error_sign = true,
+      -- animation related
+      duration = 0,
+      delay = 0,
     },
     indent = {
       enable = true,
@@ -60,57 +68,3 @@ function M.config()
 end
 
 return M
-
---[[ local M = {}
-
-function M.setup()
-	return {
-		"lukas-reineke/indent-blankline.nvim",
-		config = M.config,
-	}
-end
-
-function M.config()
-	require("indent_blankline").setup({
-		char = "│", -- ┊ ┆ 
-		context_char = "│", -- "▎",
-		show_foldtext = false,
-		char_priority = 12,
-		show_trailing_blankline_indent = false,
-		buftype_exclude = { "terminal", "telescope", "nofile" },
-		filetype_exclude = {
-			"dbout",
-			"neo-tree-popup",
-			"log",
-			"gitcommit",
-			"txt",
-			"help",
-			"NvimTree",
-			"git",
-			"flutterToolsOutline",
-			"undotree",
-			"markdown",
-			"norg",
-			"org",
-			"orgagenda",
-			"", -- for all buffers without a file type
-		},
-		show_current_context = true,
-		show_current_context_start = true,
-		show_current_context_start_on_current_line = false,
-		show_end_of_line = false,
-		show_first_indent_level = true,
-		space_char_blankline = " ",
-		-- use_treesitter = true,
-	})
-end
-
-M.highlights = {
-	-- IndentBlanklineChar = { fg = theme.ui.whitespace },
-	-- IndentBlanklineSpaceChar = { fg = theme.ui.whitespace },
-	-- IndentBlanklineSpaceCharBlankline = { fg = theme.ui.whitespace },
-	IndentBlanklineContextChar = { fg = "#938AA9" },
-	IndentBlanklineContextStart = { sp = "#938AA9", underline = true },
-}
-
-return M ]]
