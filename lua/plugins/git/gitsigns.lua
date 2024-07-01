61b8a1390facc1ca69b542c42f1ca007831194cc
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-07-01 09:11:20 -0300

    chore(gitsigns): remove deprecated highlights

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -38,33 +38,6 @@ function M.config()
 	end
 
 	require('gitsigns').setup({
-		signs = {
-			add = { hl = 'GitSignsAdd', text = '▍', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-			change = {
-				hl = 'GitSignsChange',
-				text = '▍',
-				numhl = 'GitSignsChangeNr',
-				linehl = 'GitSignsChangeLn',
-			},
-			delete = {
-				hl = 'GitSignsDelete',
-				text = '▸',
-				numhl = 'GitSignsDeleteNr',
-				linehl = 'GitSignsDeleteLn',
-			},
-			topdelete = {
-				hl = 'GitSignsDelete',
-				text = '▾',
-				numhl = 'GitSignsDeleteNr',
-				linehl = 'GitSignsDeleteLn',
-			},
-			changedelete = {
-				hl = 'GitSignsChange',
-				text = '▍',
-				numhl = 'GitSignsChangeNr',
-				linehl = 'GitSignsChangeLn',
-			},
-		},
 		signcolumn = true,
 		numhl = false,
 		linehl = false,
