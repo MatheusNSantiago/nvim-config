30368013f365019b558770ab5c6938e9fef7ef82
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-07-01 09:11:01 -0300

    chore(vevn-selector): remove nvim-dap-python dependency

--- lua/plugins/dev/venv-selector.lua
+++ lua/plugins/dev/venv-selector.lua
@@ -18,9 +18,7 @@ function M.setup()
     dependencies = {
       'neovim/nvim-lspconfig',
       'nvim-telescope/telescope.nvim',
-      'mfussenegger/nvim-dap-python',
     },
-    branch = 'regexp', -- This is the regexp branch, use this for the new version
     ft = 'python',
     branch = 'regexp', -- This is the regexp branch, use this for the new version
     config = M.config,
