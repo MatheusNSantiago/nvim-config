69803ed672f98db4b1a8e462fca0631a90d3d914
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-10-07 21:28:37 -0300

    feat(treesitter-context): reduce max lines for showing context

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,7 +1,7 @@
 local keymap = utils.api.keymap
 
 keymap('n', '<leader>it', ':InspectTree<CR>:vertical resize 50<CR><c-w>w')
-keymap('n', '<leader>tsu', ':TSUpdate foo<CR>')
+keymap('n', '<leader>tsu', ':TSUpdate copybook<CR>')
 
 --  ╭──────────────────────────────────────────────────────────╮
 --  │                          Geral                           │
