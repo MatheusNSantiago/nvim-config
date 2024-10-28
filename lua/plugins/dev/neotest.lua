e8c230e0446f056abb9b5d8b0965440e40db8dad
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-10-28 18:12:22 -0300

    feat(neotest): add keymaps to run ad toggle summary

--- lua/plugins/dev/neotest.lua
+++ lua/plugins/dev/neotest.lua
@@ -1,175 +1,199 @@
 local M = {}
 
 function M.setup()
-  return {
-    'nvim-neotest/neotest',
-    dependencies = {
-      'nvim-neotest/nvim-nio',
-      'nvim-lua/plenary.nvim',
-      'antoinemadec/FixCursorHold.nvim',
-      'nvim-treesitter/nvim-treesitter',
-      --  ─────────────────────────────
-      'rcasia/neotest-java',
-    },
-    config = M.config,
-  }
+	return {
+		'nvim-neotest/neotest',
+		dependencies = {
+			'nvim-neotest/nvim-nio',
+			'nvim-lua/plenary.nvim',
+			'antoinemadec/FixCursorHold.nvim',
+			'nvim-treesitter/nvim-treesitter',
+			--  ─────────────────────────────
+			'nvim-neotest/neotest-jest',
+		},
+		config = M.config,
+
+		keys = {
+			{ '<leader>tr', function() M.run_tests_on_file() end },
+			{ '<leader>ts', function() M.toggle_summary() end },
+		},
+	}
 end
 
 function M.config()
-  require('neotest').setup({
-    adapters = {
-      require('neotest-java')({
-        ignore_wrapper = true, -- whether to ignore maven/gradle wrapper
-        -- junit_jar = 'path/to/junit-standalone.jar',
-        -- default: .local/share/nvim/neotest-java/junit-platform-console-standalone-[version].jar
-      }),
-    },
-    benchmark = {
-      enabled = true,
-    },
-    consumers = {},
-    default_strategy = 'integrated',
-    diagnostic = {
-      enabled = true,
-      severity = 1,
-    },
-    discovery = {
-      concurrent = 0,
-      enabled = true,
-    },
-    floating = {
-      border = 'rounded',
-      max_height = 0.6,
-      max_width = 0.6,
-      options = {},
-    },
-    highlights = {
-      adapter_name = 'NeotestAdapterName',
-      border = 'NeotestBorder',
-      dir = 'NeotestDir',
-      expand_marker = 'NeotestExpandMarker',
-      failed = 'NeotestFailed',
-      file = 'NeotestFile',
-      focused = 'NeotestFocused',
-      indent = 'NeotestIndent',
-      marked = 'NeotestMarked',
-      namespace = 'NeotestNamespace',
-      passed = 'NeotestPassed',
-      running = 'NeotestRunning',
-      select_win = 'NeotestWinSelect',
-      skipped = 'NeotestSkipped',
-      target = 'NeotestTarget',
-      test = 'NeotestTest',
-      unknown = 'NeotestUnknown',
-      watching = 'NeotestWatching',
-    },
-    icons = {
-      child_indent = '│',
-      child_prefix = '├',
-      collapsed = '─',
-      expanded = '╮',
-      failed = '',
-      final_child_indent = ' ',
-      final_child_prefix = '╰',
-      non_collapsible = '─',
-      notify = '',
-      passed = '',
-      running = '',
-      running_animated = { '/', '|', '\\', '-', '/', '|', '\\', '-' },
-      skipped = '',
-      unknown = '',
-      watching = '',
-    },
-    jump = {
-      enabled = true,
-    },
-    log_level = 3,
-    output = {
-      enabled = true,
-      open_on_run = 'short',
-    },
-    output_panel = {
-      enabled = true,
-      open = 'botright split | resize 15',
-    },
-    projects = {},
-    quickfix = {
-      enabled = true,
-      open = false,
-    },
-    run = {
-      enabled = true,
-    },
-    running = {
-      concurrent = true,
-    },
-    state = {
-      enabled = true,
-    },
-    status = {
-      enabled = true,
-      signs = true,
-      virtual_text = false,
-    },
-    strategies = {
-      integrated = {
-        height = 40,
-        width = 120,
-      },
-    },
-    summary = {
-      animated = true,
-      enabled = true,
-      expand_errors = true,
-      follow = true,
-      mappings = {
-        attach = 'a',
-        clear_marked = 'M',
-        clear_target = 'T',
-        debug = 'd',
-        debug_marked = 'D',
-        expand = { '<CR>', '<2-LeftMouse>' },
-        expand_all = 'e',
-        help = '?',
-        jumpto = 'i',
-        mark = 'm',
-        next_failed = 'J',
-        output = 'o',
-        prev_failed = 'K',
-        run = 'r',
-        run_marked = 'R',
-        short = 'O',
-        stop = 'u',
-        target = 't',
-        watch = 'w',
-      },
-      open = 'botright vsplit | vertical resize 50',
-    },
-    watch = {
-      enabled = true,
-      symbol_queries = {
-        -- elixir = <function 1>,
-        go =
-        '        ;query\n        ;Captures imported types\n        (qualified_type name: (type_identifier) @symbol)\n        ;Captures package-local and built-in types\n        (type_identifier)@symbol\n        ;Captures imported function calls and variables/constants\n        (selector_expression field: (field_identifier) @symbol)\n        ;Captures package-local functions calls\n        (call_expression function: (identifier) @symbol)\n      ',
-        haskell =
-        '        ;query\n        ;explicit import\n        ((import_item [(variable)]) @symbol)\n        ;symbols that may be imported implicitly\n        ((type) @symbol)\n        (qualified_variable (variable) @symbol)\n        (exp_apply (exp_name (variable) @symbol))\n        ((constructor) @symbol)\n        ((operator) @symbol)\n      ',
-        javascript =
-        '  ;query\n  ;Captures named imports\n  (import_specifier name: (identifier) @symbol)\n  ;Captures default import\n  (import_clause (identifier) @symbol)\n  ;Capture require statements\n  (variable_declarator \n  name: (identifier) @symbol\n  value: (call_expression (identifier) @function  (#eq? @function "require")))\n  ;Capture namespace imports\n  (namespace_import (identifier) @symbol)\n',
-        lua =
-        '        ;query\n        ;Captures module names in require calls\n        (function_call\n          name: ((identifier) @function (#eq? @function "require"))\n          arguments: (arguments (string) @symbol))\n      ',
-        python =
-        "        ;query\n        ;Captures imports and modules they're imported from\n        (import_from_statement (_ (identifier) @symbol))\n        (import_statement (_ (identifier) @symbol))\n      ",
-        ruby =
-        '        ;query\n        ;rspec - class name\n        (call\n          method: (identifier) @_ (#match? @_ "^(describe|context)")\n          arguments: (argument_list (constant) @symbol )\n        )\n\n        ;rspec - namespaced class name\n        (call\n          method: (identifier)\n          arguments: (argument_list\n            (scope_resolution\n              name: (constant) @symbol))\n        )\n      ',
-        rust =
-        '        ;query\n        ;submodule import\n        (mod_item\n          name: (identifier) @symbol)\n        ;single import\n        (use_declaration\n          argument: (scoped_identifier\n            name: (identifier) @symbol))\n        ;import list\n        (use_declaration\n          argument: (scoped_use_list\n            list: (use_list\n                [(scoped_identifier\n                   path: (identifier)\n                   name: (identifier) @symbol)\n                 ((identifier) @symbol)])))\n        ;wildcard import\n        (use_declaration\n          argument: (scoped_use_list\n            path: (identifier)\n            [(use_list\n              [(scoped_identifier\n                path: (identifier)\n                name: (identifier) @symbol)\n                ((identifier) @symbol)\n              ])]))\n      ',
-        tsx =
-        '  ;query\n  ;Captures named imports\n  (import_specifier name: (identifier) @symbol)\n  ;Captures default import\n  (import_clause (identifier) @symbol)\n  ;Capture require statements\n  (variable_declarator \n  name: (identifier) @symbol\n  value: (call_expression (identifier) @function  (#eq? @function "require")))\n  ;Capture namespace imports\n  (namespace_import (identifier) @symbol)\n',
-        typescript =
-        '  ;query\n  ;Captures named imports\n  (import_specifier name: (identifier) @symbol)\n  ;Captures default import\n  (import_clause (identifier) @symbol)\n  ;Capture require statements\n  (variable_declarator \n  name: (identifier) @symbol\n  value: (call_expression (identifier) @function  (#eq? @function "require")))\n  ;Capture namespace imports\n  (namespace_import (identifier) @symbol)\n',
-      },
-    },
-  })
+	M.setup_smart_close_autocommand()
+
+	require('neotest').setup({
+		adapters = {
+			require('neotest-jest')({
+				jestCommand = 'npm test --',
+				jestConfigFile = 'custom.jest.config.ts',
+				env = { CI = true },
+				cwd = function(_) return vim.fn.getcwd() end,
+			}),
+		},
+		benchmark = { enabled = true },
+		consumers = {},
+		default_strategy = 'integrated',
+		diagnostic = { enabled = true, severity = 1 },
+		discovery = { concurrent = 0, enabled = true },
+		floating = {
+			border = 'rounded',
+			max_height = 0.6,
+			max_width = 0.6,
+			options = {},
+		},
+		highlights = {
+			adapter_name = 'NeotestAdapterName',
+			border = 'NeotestBorder',
+			dir = 'NeotestDir',
+			expand_marker = 'NeotestExpandMarker',
+			failed = 'NeotestFailed',
+			file = 'NeotestFile',
+			focused = 'NeotestFocused',
+			indent = 'NeotestIndent',
+			marked = 'NeotestMarked',
+			namespace = 'NeotestNamespace',
+			passed = 'NeotestPassed',
+			running = 'NeotestRunning',
+			select_win = 'NeotestWinSelect',
+			skipped = 'NeotestSkipped',
+			target = 'NeotestTarget',
+			test = 'NeotestTest',
+			unknown = 'NeotestUnknown',
+			watching = 'NeotestWatching',
+		},
+		icons = {
+			child_indent = '│',
+			child_prefix = '├',
+			collapsed = '─',
+			expanded = '╮',
+			failed = '',
+			final_child_indent = ' ',
+			final_child_prefix = '╰',
+			non_collapsible = '─',
+			notify = '',
+			passed = '',
+			running = '',
+			running_animated = { '/', '|', '\\', '-', '/', '|', '\\', '-' },
+			skipped = '',
+			unknown = '',
+			watching = '',
+		},
+		jump = { enabled = true },
+		log_level = 3,
+		output = { enabled = true, open_on_run = 'short' },
+		output_panel = {
+			enabled = true,
+			open = 'botright split | resize 15',
+		},
+		projects = {},
+		quickfix = { enabled = true, open = false },
+		run = { enabled = true },
+		running = { concurrent = true },
+		state = { enabled = true },
+		status = {
+			enabled = true,
+			signs = true,
+			virtual_text = false,
+		},
+		strategies = {
+			integrated = { height = 40, width = 120 },
+		},
+		summary = {
+			animated = true,
+			count = true,
+			enabled = true,
+			expand_errors = true,
+			follow = true,
+			mappings = {
+				attach = 'a',
+				clear_marked = 'M',
+				clear_target = 'T',
+				debug = 'd',
+				debug_marked = 'D',
+				expand = { '<CR>', '<2-LeftMouse>' },
+				expand_all = 'e',
+				help = '?',
+				jumpto = 'i',
+				mark = 'm',
+				next_failed = 'J',
+				output = 'o',
+				prev_failed = 'K',
+				run = 'r',
+				run_marked = 'R',
+				short = 'O',
+				stop = 'u',
+				target = 't',
+				watch = 'w',
+			},
+			open = 'botright vsplit | vertical resize 50',
+		},
+	})
+end
+
+M.is_first_time_opening_summary = true
+M.toggle_summary = function()
+	local summary_is_closed = M._get_summary_window() == nil
+
+	if summary_is_closed then
+		require('neotest').summary.open() -- abre
+		vim.api.nvim_set_current_win(M._get_summary_window()) -- coloca o cursor no sumário
+
+		-- Existe demora para abrir o sumário pela primeira vez
+		-- Para botar o cursor no sumário do teste mais perto do teste do buffer principal, precisamos
+		-- fazer um hackzinho de sair e voltar do buffer
+		if M.is_first_time_opening_summary then -- faz o hack
+			vim.defer_fn(function()
+				vim.cmd('wincmd w') -- Volta para o buffer principal
+
+				-- Espera um pouco para o neotest gravar a localização
+				-- Ao voltar p/ o sumário, o cursor será colocado no teste mais perto
+				vim.defer_fn(function()
+					vim.cmd('wincmd w') -- Volta pro sumário (mouse no teste mais próximo)
+				end, 100)
+			end, 1000)
+			M.is_first_time_opening_summary = false
+		end
+	else
+		require('neotest').summary.close()
+	end
+end
+
+M._get_summary_window = function()
+	local wins = vim.api.nvim_list_wins()
+	for _, win in ipairs(wins) do
+		local buf = vim.fn.winbufnr(win)
+		if vim.bo[buf].ft == 'neotest-summary' then return win end
+	end
+end
+
+M.run_tests_on_file = function()
+	require('neotest').run.run(vim.fn.expand('%'))
+
+	local summary_win = M._get_summary_window()
+
+	local summary_pannel_is_open = summary_win ~= nil
+	if summary_pannel_is_open then --
+		vim.api.nvim_set_current_win(summary_win)
+	else
+		M.toggle_summary()
+	end
+end
+
+---@see https://github.com/akinsho/dotfiles/blob/4831310bbf0ecfc5d563a21494353216c1fc0693/.config/nvim/plugin/autocommands.lua#L86C1-L97C1b
+M.setup_smart_close_autocommand = function()
+	utils.api.augroup('smart_close_neotest', {
+		desc = 'Fecha a janela do neotest com q',
+		event = { 'FileType' },
+		pattern = { 'neotest-*' },
+		command = function(args)
+			utils.api.keymap('n', 'q', function() vim.api.nvim_win_close(0, true) end, {
+				buffer = args.buf,
+				nowait = true,
+			})
+		end,
+	})
 end
 
 return M
