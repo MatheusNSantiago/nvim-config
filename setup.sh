cfd34fdb554eba11d4ae5556ca335c6b2e140ad9
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-11-05 18:47:54 -0300

    feat(setup): add npm, nodejs, gcc, make

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
+sudo pacman -S --noconfirm git npm nodejs gcc make
+#  ╾───────────────────────────────────────────────────────────────────────────────────╼
+
+# Linters
 bun i -g eslint_d jsonlint
+
+# TUI
+sudo pacman -S lazygit
