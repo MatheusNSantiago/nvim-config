66fd149ec3d88484d65a3676f61b62644ca994ac
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-11-20 22:45:27 -0300

    feat(dev/gitsigns): update mappings

--- lua/plugins/dev/gitsigns.lua
+++ lua/plugins/dev/gitsigns.lua
@@ -8,58 +8,49 @@ function M.setup()
 end
 
 function M.config()
-	local function on_attach(bufnr)
-		local gs = package.loaded.gitsigns
+	local gs = require('gitsigns')
 
-		local function map(mode, l, r, opts)
-			opts = opts or {}
-			opts.buffer = bufnr
-			vim.keymap.set(mode, l, r, opts)
-		end
-
-		-- Navigation
-		map('n', ']h', function()
-			if vim.wo.diff then return ']h' end
-			vim.schedule(function() gs.next_hunk() end)
-			return '<Ignore>'
-		end, { expr = true })
-
-		map('n', '[h', function()
-			if vim.wo.diff then return '[h' end
-			vim.schedule(function() gs.prev_hunk() end)
-			return '<Ignore>'
-		end, { expr = true })
-
-		-- Actions
-		map({ 'n', 'v' }, 'ha', gs.stage_hunk, { desc = 'gitsigns: [H]unk [A]dd' })
-		map({ 'n', 'v' }, 'hr', gs.reset_hunk, { desc = 'gitsigns: [H]unk [R]eset' })
-		map('n', 'hp', gs.preview_hunk, { desc = 'gitsigns: [H]unk [P]review' })
-		map('n', 'hb', gs.blame_line, { desc = 'gitsigns: [H]unk [B]lame' })
-	end
-
-	require('gitsigns').setup({
-		signcolumn = true,
-		numhl = false,
-		linehl = false,
-		word_diff = false,
+	gs.setup({
+		signs = {
+			add = { text = '┃' },
+			change = { text = '┃' },
+			delete = { text = '_' },
+			topdelete = { text = '‾' },
+			changedelete = { text = '~' },
+			untracked = { text = '┆' },
+		},
+		signs_staged = {
+			add = { text = '┃' },
+			change = { text = '┃' },
+			delete = { text = '_' },
+			topdelete = { text = '‾' },
+			changedelete = { text = '~' },
+			untracked = { text = '┆' },
+		},
+		signs_staged_enable = true,
+		signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+		linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+		word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 		watch_gitdir = {
-			interval = 1000,
 			follow_files = true,
 		},
-		attach_to_untracked = true,
+		auto_attach = true,
+		attach_to_untracked = false,
+		-- attach_to_untracked = true,
 		current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 		current_line_blame_opts = {
 			virt_text = true,
 			virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
 			delay = 1000,
 			ignore_whitespace = false,
-			-- virt_text_priority = 100,
+			virt_text_priority = 100,
+			use_focus = true,
 		},
-		current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+		current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
 		sign_priority = 6,
+		update_debounce = 100,
 		status_formatter = nil, -- Use default
-		update_debounce = 200,
-		max_file_length = 40000,
+		max_file_length = 40000, -- Disable if file is longer than this (in lines)
 		preview_config = {
 			-- Options passed to nvim_open_win
 			border = 'rounded',
@@ -68,8 +59,58 @@ function M.config()
 			row = 0,
 			col = 1,
 		},
-		on_attach = on_attach,
+		on_attach = function(bufnr)
+			local function map(mode, l, r, opts)
+				opts = opts or {}
+				opts.buffer = bufnr
+				vim.keymap.set(mode, l, r, opts)
+			end
+
+			-- Navigation
+			map('n', ']h', function()
+				if vim.wo.diff then
+					vim.cmd.normal({ ']h', bang = true })
+				else
+					gs.nav_hunk('next')
+				end
+			end)
+
+			map('n', '[h', function()
+				if vim.wo.diff then
+					return vim.cmd.normal({ '[h', bang = true })
+				else
+					gs.nav_hunk('prev')
+				end
+			end)
+
+			-- Actions
+			map({ 'n', 'v' }, '<leader>ha', function()
+				if vim.fn.mode() == 'n' then
+					gs.stage_hunk()
+				else
+					gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+				end
+			end, { desc = 'gitsigns: [H]unk [A]dd' })
+
+			map('v', '<leader>hr', function()
+				if vim.fn.mode() == 'n' then
+					gs.reset_hunk()
+				else
+					gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+				end
+			end, { desc = 'gitsigns: [H]unk [R]eset' })
+
+			map('n', '<leader>hu', gs.undo_stage_hunk)
+			map('n', '<leader>ht', gs.toggle_deleted, { desc = 'gitsigns: [H]unk [T]oggle Deleted' })
+
+			map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+		end,
 	})
 end
+
+M.highlights = {
+	DiffAdd = { bg = '#002800' },
+	DiffDelete = { bg = '#3f0001' },
+}
 
 return M
