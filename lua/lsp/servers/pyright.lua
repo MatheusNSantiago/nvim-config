cbcf7e2be334f901fc470890b07e1033a8763a1c
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2024-12-09 01:25:54 -0300

    fix(lsp/servers): disable diagnostics for pyright

--- lua/lsp/servers/pyright.lua
+++ lua/lsp/servers/pyright.lua
@@ -1,26 +1,33 @@
-local util = require('lspconfig.util')
 local M = {}
 
-M.root_dir = util.root_pattern(
-  'pyproject.toml',
-  'requirements.txt',
-  '.venv',
-  '.git',
-  'setup.py',
-  'setup.cfg',
-  'pyrightconfig.json'
+M.root_dir = require('lspconfig.util').root_pattern(
+	'pyproject.toml',
+	'requirements.txt',
+	'.venv',
+	'.git',
+	'setup.py',
+	'setup.cfg',
+	'pyrightconfig.json'
 )
 
+M.handlers = {
+	['textDocument/publishDiagnostics'] = function() end,
+}
+
 M.settings = {
-  pyright = { autoImportCompletion = true },
-  python = {
-    analysis = {
-      autoSearchPaths = true,
-      diagnosticMode = 'openFilesOnly',
-      useLibraryCodeForTypes = true,
-      typeCheckingMode = 'off',
-    },
-  },
+	pyright = {
+		autoImportCompletion = true,
+		disableOrganizeImports = true, -- Prefiro o do ruff
+	},
+	python = {
+		analysis = {
+			autoSearchPaths = true,
+			-- diagnosticMode = 'openFilesOnly',
+			diagnosticMode = 'workspace',
+			useLibraryCodeForTypes = true,
+			typeCheckingMode = 'off',
+		},
+	},
 }
 
 return M
