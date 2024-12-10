f9df170f111d49048f521d65e9c5279b14a63591
F7023235 Matheus Nilo Santiago <matheus.santiago@bb.com.br>
2024-12-10 10:32:50 -0300

    feat(edt/nvim-ufo): remove foldcolumn

--- lua/plugins/edit/nvim-ufo.lua
+++ lua/plugins/edit/nvim-ufo.lua
@@ -12,7 +12,7 @@ end
 function M.config()
 	local ufo = require('ufo')
 
-	vim.o.foldcolumn = '1' -- '0' is not bad
+	vim.o.foldcolumn = '0' -- '0' is not bad
 	vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 	vim.o.foldlevelstart = 99
 	vim.o.foldenable = true
