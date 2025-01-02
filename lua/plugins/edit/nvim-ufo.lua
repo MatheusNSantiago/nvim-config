1eca94011015510939ad19886a674e3407f0faf8
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2025-01-01 23:13:08 -0300

    fix(edit/nvim-ufo): set option values at init

--- lua/plugins/edit/nvim-ufo.lua
+++ lua/plugins/edit/nvim-ufo.lua
@@ -1,10 +1,9 @@
 local M = {}
 
 function M.setup()
-	return {
+	return { ---@type LazyPluginSpec
 		'kevinhwang91/nvim-ufo',
 		config = M.config,
-		dependencies = { 'kevinhwang91/promise-async' },
 		keys = {
 			{
 				'za',
@@ -17,15 +16,16 @@ function M.setup()
 			{ 'zR', function() require('ufo').openAllFolds() end, desc = 'Open all folds' },
 			{ 'zM', function() require('ufo').closeAllFolds() end, desc = 'Close all folds' },
 		},
+		init = function()
+			vim.o.foldcolumn = '0' -- '0' is not bad
+			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+		end,
 	}
 end
 
 function M.config()
-	vim.o.foldcolumn = '0' -- '0' is not bad
-	vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-	vim.o.foldlevelstart = 99
-	vim.o.foldenable = true
-
 	require('ufo').setup({
 		open_fold_hl_timeout = 400,
 		close_fold_kinds_for_ft = { default = {} },
