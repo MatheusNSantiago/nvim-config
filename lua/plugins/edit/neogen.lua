c57101dfdddfda93261306242537fda523c21ac3
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2025-02-17 15:17:17 -0300

    feat(edit/neogen): make lazy load

--- lua/plugins/edit/neogen.lua
+++ lua/plugins/edit/neogen.lua
@@ -1,51 +1,50 @@
 local M = {}
 
 function M.setup()
-  return {
-    'danymat/neogen',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    lazy = true,
-    config = M.config,
-  }
+	return { ---@type LazyPluginSpec{
+		'danymat/neogen',
+		config = M.config,
+		cmd = { 'Neogen' },
+	}
 end
 
 function M.config()
-  require('neogen').setup({
-    enabled = true,
-    snippet_engine = 'luasnip',
-    languages = {
-      lua = {
-        template = {
-          annotation_convention = 'emmylua',
-        },
-      },
-      python = {
-        template = {
-          annotation_convention = 'google_docstrings',
-        },
-      },
-      rust = {
-        template = {
-          annotation_convention = 'rustdoc',
-        },
-      },
-      javascript = {
-        template = {
-          annotation_convention = 'jsdoc',
-        },
-      },
-      typescript = {
-        template = {
-          annotation_convention = 'tsdoc',
-        },
-      },
-      typescriptreact = {
-        template = {
-          annotation_convention = 'tsdoc',
-        },
-      },
-    },
-  })
+	require('neogen').setup({
+		enabled = true,
+		snippet_engine = 'luasnip',
+		languages = {
+			lua = {
+				template = {
+					annotation_convention = 'emmylua',
+				},
+			},
+			python = {
+				template = {
+					annotation_convention = 'google_docstrings',
+				},
+			},
+			rust = {
+				template = {
+					annotation_convention = 'rustdoc',
+				},
+			},
+			javascript = {
+				template = {
+					annotation_convention = 'jsdoc',
+				},
+			},
+			typescript = {
+				template = {
+					annotation_convention = 'tsdoc',
+				},
+			},
+			typescriptreact = {
+				template = {
+					annotation_convention = 'tsdoc',
+				},
+			},
+		},
+	})
 end
 
 return M
