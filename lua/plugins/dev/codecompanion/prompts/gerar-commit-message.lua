7b1a430be89ad979ecb52b80afc594c7c362b471
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2025-03-25 08:44:36 -0300

    feat(codecompanion): improve commit message generation prompt
    This commit improves the commit message generation prompt in codecompanion by:
    
    - Removing the slash command behavior, making it a regular prompt.
    - Adding `ignore_system_prompt` to prevent conflicts.
    - Automatically triggering `Git commit` after generating the commit message.
    
    These changes streamline the commit message generation process and improve the user experience.

--- lua/plugins/dev/codecompanion/prompts/gerar-commit-message.lua
+++ lua/plugins/dev/codecompanion/prompts/gerar-commit-message.lua
@@ -1,30 +1,34 @@
 return {
-  strategy = "chat",
-  description = "Generate a commit message",
-  opts = {
-    index = 10,
-    is_default = false,
-    is_slash_cmd = true,
-    auto_submit = true,
+	strategy = 'chat',
+	description = 'Generate a commit message',
+	opts = {
+		index = 10,
+		is_default = false,
+		is_slash_cmd = false,
+		auto_submit = true,
 		short_name = 'gerar_mensagem_de_commit',
-  },
-  prompts = {
-    {
-      role = "user",
-      content = function()
-        return string.format(
-          [[Você é um especialista em seguir a especificação do Conventional Commits. Dado o diff do git abaixo, gere uma mensagem de commit para mim:
+		ignore_system_prompt = true,
+	},
+	prompts = {
+		{
+			role = 'user',
+			content = function()
+				local results = string.format(
+					[[Você é um especialista em seguir a especificação do Conventional Commits. Dado o diff do git abaixo, gere uma mensagem de commit para mim:
 
 ```diff
 %s
 ```
 ]],
-          vim.fn.system("git diff --no-ext-diff --staged")
-        )
-      end,
-      opts = {
-        contains_code = true,
-      },
-    },
-  },
+					vim.fn.system('git diff --no-ext-diff --staged')
+				)
+				vim.cmd([[Git commit]])
+
+				return results
+			end,
+			opts = {
+				contains_code = true,
+			},
+		},
+	},
 }
