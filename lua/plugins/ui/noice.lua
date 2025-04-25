b1c129a38c17947a33691cfd537067b2fb57115d
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2025-04-25 08:57:39 -0300

    feat(noice): enable lsp markdown overrides and hover, add doc border
    This commit enables the following features in the noice plugin:
    
    - Enables LSP markdown overrides, allowing Noice to handle the formatting of LSP markdown.
    - Enables hover messages, displaying information on hover.
    - Adds a border to hover documentation and signature help, improving readability.

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -141,11 +141,11 @@ M.lsp = {
 	progress = { enabled = false },
 	-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 	override = {
-		['vim.lsp.util.convert_input_to_markdown_lines'] = false, -- override the default lsp markdown formatter with Noice
-		['vim.lsp.util.stylize_markdown'] = false, -- override the lsp markdown formatter with Noice
-		['cmp.entry.get_documentation'] = false, --       -- override cmp documentation with Noice (needs the other options to work)
+		['vim.lsp.util.convert_input_to_markdown_lines'] = true, -- override the default lsp markdown formatter with Noice
+		['vim.lsp.util.stylize_markdown'] = true, -- override the lsp markdown formatter with Noice
+		['cmp.entry.get_documentation'] = true, --       -- override cmp documentation with Noice (needs the other options to work)
 	},
-	hover = { enabled = false },
+	hover = { enabled = true },
 	signature = { enabled = false },
 	message = { -- Messages shown by lsp servers
 		enabled = true,
@@ -159,7 +159,7 @@ M.presets = {
 	command_palette = false, -- position the cmdline and popupmenu together
 	long_message_to_split = false, -- long messages will be sent to a split
 	inc_rename = false, -- enables an input dialog for inc-rename.nvim
-	lsp_doc_border = false, -- add a border to hover docs and signature help
+	lsp_doc_border = true, -- add a border to hover docs and signature help
 }
 
 M.routes = { -- Skip Messages
